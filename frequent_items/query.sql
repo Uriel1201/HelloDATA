88dc5233cfeeb319bd2fb68bb4559afa8cb3d039
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-28 07:04:36 -0600

    Update query.sql

--- frequent_items/query.sql
+++ frequent_items/query.sql
@@ -1,3 +1,11 @@
+/*
+02. Changes in Net Worth
+
+Writing a query to return the change in net worth for each user, 
+ordered by decreasing net change. */
+
+
+/*Querying original data*/
 SELECT
     *
 FROM
