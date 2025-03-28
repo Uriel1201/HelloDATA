63208c0267d8d4169003b74cc2f77940b56cd37f
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-27 23:02:19 -0600

    Update query.sql

--- changes_net_worth/query.sql
+++ changes_net_worth/query.sql
@@ -5,3 +5,8 @@ Writing a query to return the change in net worth for each user,
 ordered by decreasing net change. */
 
 SELECT * FROM TRANSACTIONS_P2; 
+
+CREATE TABLE sendings_p2 as 
+   SELECT sender, SUM(amount) as sendings_p2 
+      FROM TRANSACTIONS_P2
+      GROUP BY sender;
