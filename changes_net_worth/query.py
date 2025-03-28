64efe6f506751c98e3282d7009f08de02dc0ca58
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-28 06:30:53 -0600

    Update and rename 02_query.py to query.py

---
+++ changes_net_worth/query.py
@@ -0,0 +1,63 @@
+"""
+# 2. Changes in Net Worth
+*From the following table of transactions between two users, 
+write a query to return the change in net worth for each user,
+ordered by decreasing net change.*
+"""
+
+
+# pip install SQLAlchemy
+# pip install cx_Oracle
+
+import pandas as pd
+import numpy  as np
+# import cx_Oracle
+# import sqlalchemy
+# from sqlalchemy.exc import SQLAlchemyError
+
+'''
+try:
+  engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+
+  table = """select * from transactions_p2""";
+  transactions = pd.read_sql(table, engine)
+  transactions
+
+except SQLAlchemyError as e:
+  print(e)
+'''
+
+
+data = {'Sender'            : [5, 1, 2, 2, 3, 3, 1],
+        'Receiver'          : [2, 3, 1, 3, 1, 2, 4],
+        'Amount'            : [10, 15, 20, 25, 20, 15, 5],
+        'Transaction_Date'  : ['12-FEB-20',
+                               '13-FEB-20',
+                               '13-FEB-20',
+                               '14-FEB-20',
+                               '15-FEB-20',
+                               '15-FEB-20',
+                               '16-FEB-20']
+        }
+
+transactions = pd.DataFrame(data)
+transactions['Transaction_Date'] = pd.to_datetime(transactions['Transaction_Date'])
+
+
+net_changes = (transactions.melt(id_vars=['Amount']
+                                 ,value_vars=['Sender', 'Receiver']
+                                 ,var_name='Type'
+                                 ,value_name='User_id'
+                            )
+                           .assign(Amount=lambda x:
+                                                 x['Amount'] * x['Type'].map({'Sender': -1, 'Receiver': 1})
+                            )
+                           .groupby('User_id')['Amount']
+                           .sum()
+                           .reset_index(name='Net_Changes'
+                            )
+                           .sort_values(by='Net_Changes'
+                                        , ascending=False
+                            )
+)
+net_changes
