485c66e7662294c7209fb1b34f041ce3fff8bb61
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-30 06:13:37 -0600

    Update data.sql

--- time_difference/data.sql
+++ time_difference/data.sql
@@ -5,3 +5,10 @@ writing a query to return for each user the
 time elapsed between the last action
 and the second-to-last action, in
 ascending order by user ID. */ 
+
+
+CREATE TABLE USERS_P4 (
+    ID          INTEGER,
+    ACTIONS     VARCHAR(10),
+    ACTION_DATE DATE
+);
