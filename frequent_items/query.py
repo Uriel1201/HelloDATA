c9f3f24436fd680fae6f090146e84ffd8cf24198
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-30 05:39:16 -0600

    Update query.py

--- frequent_items/query.py
+++ frequent_items/query.py
@@ -20,6 +20,22 @@ try:
     items = pd.read_sql(table, engine)
     print(items)
 
+    # m_f_i: "Most Frequent Item On Each Date"
+    f_1=lambda x:x.eq(x.max(axis=1),axis=0)
+    m_f_i=(pd.get_dummies(items['Item'])
+             .groupby(items['Dates'])
+             .sum()
+             .pipe(f_1)
+             .reset_index()
+             .melt(id_vars=['Dates']
+                   ,var_name='Item'
+                   ,value_name='Was_Top'
+                  )
+             .sort_values(by='Dates')
+          )
+    m_f_i[m_f_i['Was_Top']][['Dates','Item']]
+     
+
 except SQLAlchemyError as e:
     print(f"Error al conectar a la base de datos o al ejecutar la consulta: {e}")
 
