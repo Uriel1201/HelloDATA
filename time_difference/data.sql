f8ebd845bf69a7c20b9504d0533a52a1a4e9070f
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-30 06:15:12 -0600

    Update data.sql

--- time_difference/data.sql
+++ time_difference/data.sql
@@ -12,3 +12,67 @@ CREATE TABLE USERS_P4 (
     ACTIONS     VARCHAR(10),
     ACTION_DATE DATE
 );
+
+
+INSERT INTO USERS_P4
+    WITH NAMES AS (
+        SELECT
+            1,
+            'Start',
+            '13-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'Cancel',
+            '13-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            2,
+            'Start',
+            '11-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            2,
+            'Publish',
+            '14-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'Start',
+            '15-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'Cancel',
+            '15-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            4,
+            'Start',
+            '18-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'Publish',
+            '19-feb-20'
+        FROM
+            DUAL
+    )
+    SELECT
+        *
+    FROM
+        NAMES;
