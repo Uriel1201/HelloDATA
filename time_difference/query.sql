4b0554985598ad98ad41b7ac9769a79dc1cbe4f6
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-30 06:16:33 -0600

    Update query.sql

--- time_difference/query.sql
+++ time_difference/query.sql
@@ -1,1 +1,7 @@
+/* 
+04. Time Difference Between Latest Actions
 
+writing a query to return for each user the
+time elapsed between the last action
+and the second-to-last action, in
+ascending order by user ID. */ 
