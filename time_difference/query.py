d8faa0d91c4b6ca3287acff43588cf2094b13fe1
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-30 23:10:09 -0600

    Update query.py

--- time_difference/query.py
+++ time_difference/query.py
@@ -21,6 +21,27 @@ try:
     print(users)
 
     
+    #e_t: 'Elapsed time between the last two actions'
+    e_t=(users.sort_values(by=['User_id', 'Action_date']
+                           ,ascending=[True,False]
+               )
+              .groupby('User_id'
+                       ,as_index=False
+               )
+              .agg(Ranked_one=('Action_date'
+                               ,'first'
+                   )
+                   ,Ranked_two=('Action_date'
+                                ,lambda x:
+                                        x.iloc[1] if len(x)>1 else pd.NaT
+                    )
+               )
+              .assign(Elapsed_time=lambda x: 
+                                          x['Ranked_one']-x['Ranked_two']
+               )
+    )
+    e_t[['User_id','Elapsed_time']]
+    
      
 
 except SQLAlchemyError as e:
