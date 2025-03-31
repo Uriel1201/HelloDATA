d1ae80b7f8b631b13166f10c9791f484e8f0613a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-31 00:54:26 -0600

    Update query.sql

--- super_users/query.sql
+++ super_users/query.sql
@@ -2,3 +2,17 @@ SELECT
     *
 FROM
     USERS_P5;
+
+
+CREATE TABLE RANKINGS_P5
+    AS
+        SELECT
+            USER_ID,
+            TRANSACTION_DATE,
+            ROW_NUMBER()
+            OVER(PARTITION BY USER_ID
+                 ORDER BY
+                     TRANSACTION_DATE
+            ) AS RANKED_DATE
+        FROM
+            USERS_P5;
