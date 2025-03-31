afbd0a49ac314ed72051f3708ef8170c1a9fad08
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-03-31 00:35:37 -0600

    Update data.sql

--- super_users/data.sql
+++ super_users/data.sql
@@ -1,1 +1,10 @@
+/* 
+05. Super Users.
 
+A company defines its super users as
+those who have made at least two
+transactions. 
+Writing a query to return, for each user, the
+date when they become a super user, ordered by oldest super users first.
+Users who are not super users should
+also be present in the table. */
