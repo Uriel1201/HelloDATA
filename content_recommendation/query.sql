ae04b4c45dbaef17580f0896f7b27acbdf7a650f
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-04 05:25:20 -0600

    Update query.sql

--- content_recommendation/query.sql
+++ content_recommendation/query.sql
@@ -25,12 +25,13 @@ FROM
     LIKES_FRIENDS_P6;
 
 
-CREATE TABLE RECOMMENDATIONS_P6
+CREATE TABLE RECOMMENDATION_P6
     AS
         SELECT
             LF.USER_ID,
+            LF.FRIEND_LIKES,
             LI.PAGE_LIKES AS RECOMMENDED
         FROM
             LIKES_FRIENDS_P6 LF
             LEFT JOIN LIKES_P6         LI ON LF.USER_ID = LI.USER_ID
-                                     AND LF.PAGE_LIKES = LI.PAGE_LIKES;
+                                     AND LF.FRIEND_LIKES = LI.PAGE_LIKES
