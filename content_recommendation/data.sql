5189783ba868ac0b90c4dc79a62cf227cb86e8a7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-03 22:38:38 -0600

    Update data.sql

--- content_recommendation/data.sql
+++ content_recommendation/data.sql
@@ -5,3 +5,9 @@ Writing a query to return page recommendations
 to a social media user based on the
 pages that their friends have liked, but
 that they have not yet marked as liked. */
+
+
+CREATE TABLE FRIENDS_P6 (
+    USER_ID INTEGER,
+    FRIEND  INTEGER
+);
