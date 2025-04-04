621702ab9d16898bfc6f48a47d065b273097d498
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-03 22:35:01 -0600

    Update data.sql

--- content_recommendation/data.sql
+++ content_recommendation/data.sql
@@ -1,1 +1,7 @@
+/*  
+06. Content Recommendations
 
+Writing a query to return page recommendations
+to a social media user based on the
+pages that their friends have liked, but
+that they have not yet marked as liked. */
