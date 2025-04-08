71dbca329f1274ecc63d5c963c4905a439595ed7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-08 15:37:37 -0600

    Update query.sql

--- frequent_items/query.sql
+++ frequent_items/query.sql
@@ -4,98 +4,49 @@
 Writing a query to return the most frequent item
 ordered on each date. */
 
-
 /*Querying original data*/
 SELECT
     *
 FROM
     ITEMS_P3;
 
-
-/*Frequencies for items on each date*/
-CREATE TABLE FREQUENCIES_P3
-    AS
-        SELECT
-            DATES,
-            ITEM,
-            COUNT(*) AS FREQUENCY
-        FROM
-            ITEMS_P3
-        GROUP BY
-            DATES,
-            ITEM;
-
-
-/*Querying frequencies*/
-SELECT
-    *
-FROM
-    FREQUENCIES_P3;
-
-
-/*ranking items on each date*/
-CREATE TABLE ITEMS_RANKING_P3
-    AS
-        SELECT
-            DATES,
-            ITEM,
-            RANK()
-            OVER(PARTITION BY DATES
-                 ORDER BY
-                     FREQUENCY DESC
-            ) AS RANKING
-        FROM
-            FREQUENCIES_P3;
-
-
-/*Querying rankings*/
-SELECT
-    *
-FROM
-    ITEMS_RANKING_P3;
-
-
 /*Querying most frequent items on each date*/
+WITH FREQUENCIES --Frequencies for items on each date
+   (
+    DATES,
+    ITEM,
+    FREQUENCY
+) AS (
+    SELECT
+        DATES,
+        ITEM,
+        COUNT(*)
+    FROM
+        ITEMS_P3
+    GROUP BY
+        DATES,
+        ITEM
+), ITEMS_RANKING --ranking items on each date
+   (
+    DATES,
+    ITEM,
+    RANKING
+) AS (
+    SELECT
+        DATES,
+        ITEM,
+        RANK()
+        OVER(PARTITION BY DATES
+             ORDER BY
+                 FREQUENCY DESC
+        )
+    FROM
+        FREQUENCIES
+)
 SELECT
     DATES,
     ITEM
 FROM
-    ITEMS_RANKING_P3
+    ITEMS_RANKING
 WHERE
     RANKING = 1;
-
-
-
-/*
-with 
-FREQUENCIES(dates, item, frequency)
-    AS (
-        SELECT
-            DATES,
-            ITEM,
-            COUNT(*) AS FREQUENCY
-        FROM
-            ITEMS_P3
-        GROUP BY
-            DATES,
-            ITEM), 
-ITEMS_RANKING(dates, item, ranking)
-    AS (
-        SELECT
-            DATES,
-            ITEM,
-            RANK()
-            OVER(PARTITION BY DATES
-                 ORDER BY
-                     FREQUENCY DESC
-            )
-        FROM
-            FREQUENCIES_P3)
-SELECT
-    DATES,
-    ITEM
-FROM
-    ITEMS_RANKING_P3
-WHERE
-    RANKING = 1;
-*/
