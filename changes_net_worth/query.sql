abfd585a114aceb5fe033e2feb8fa2ab5ba063f6
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-08 14:22:19 -0600

    Update query.sql

--- changes_net_worth/query.sql
+++ changes_net_worth/query.sql
@@ -9,10 +9,6 @@ ordered by decreasing net change. */
 SELECT * FROM TRANSACTIONS_P2; 
 
 
-/* Table of Senders and their sended amounts*/
-/* Table of receivers and their received amounts*/
-/* Querying senders*/
-/* Querying receivers*/
 /* Querying the net change of each user*/
 WITH SENDERS_P2 (
     SENDER,
@@ -26,6 +22,46 @@ WITH SENDERS_P2 (
     GROUP BY
         SENDER
 ), RECEIVERS_P2 (
+    RECEIVER,
+    RECEIVING
+) AS (
+    SELECT
+        RECEIVER,
+        SUM(AMOUNT)
+    FROM
+        TRANSACTIONS_P2
+    GROUP BY
+        RECEIVER
+)
+SELECT
+    COALESCE(S.SENDER, R.RECEIVER)                    AS USER_ID,
+    COALESCE(R.RECEIVING, 0) - COALESCE(S.SENDING, 0) AS NET_CHANGE
+FROM
+    RECEIVERS_P2 R
+    FULL OUTER JOIN SENDERS_P2   S ON R.RECEIVER = S.SENDER
+ORDER BY
+    2 DESC;
+
+
+
+
+
+
+
+/* Querying the net change of each user*/
+WITH SENDERS_P2 
+/* Table of Senders and their sended amounts*/ (
+    SENDER,
+    SENDING
+) AS (
+    SELECT
+        SENDER,
+        SUM(AMOUNT)
+    FROM
+        TRANSACTIONS_P2
+    GROUP BY
+        SENDER
+), RECEIVERS_P2 /* Table of receivers and their received amounts*/ (
     RECEIVER,
     RECEIVING
 ) AS (
