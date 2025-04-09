4767eb7d2feec2a1570c1b576e26c199ad3c36b6
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-08 19:50:24 -0600

    Update live-sql-demo-2.sql

--- time_difference/live-sql-demo-2.sql
+++ time_difference/live-sql-demo-2.sql
@@ -1,3 +1,18 @@
+/* 
+04. Time Difference Between Latest Actions
+
+writing a query to return for each user the
+time elapsed between the last action
+and the second-to-last action, in
+ascending order by user ID. */ 
+
+
+SELECT
+    *
+FROM
+    USERS_P4;
+
+
 WITH RANKINGS (
     ID,
     ACTION_DATE,
