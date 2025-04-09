c644b3662baf6638c6882f45ecbb7e676cfb2017
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-09 01:02:52 -0600

    Update query.sql

--- super_users/query.sql
+++ super_users/query.sql
@@ -9,66 +9,50 @@ date when they become a super user, ordered by oldest super users first.
 Users who are not super users should
 also be present in the table. */
 
-
+/* Querying original data */
 SELECT
     *
 FROM
     USERS_P5;
 
-CREATE TABLE RANKINGS_P5
-    AS
-        SELECT
-            USER_ID,
-            TRANSACTION_DATE,
-            ROW_NUMBER()
-            OVER(PARTITION BY USER_ID
-                 ORDER BY
-                     TRANSACTION_DATE
-            ) AS RANKED_DATE
-        FROM
-            USERS_P5;
-
-
+/*---------------*/
+WITH RANKINGS (
+    USER_ID,
+    TRANSACTION_DATE,
+    RANKED_DATE
+) AS (
+    SELECT
+        USER_ID,
+        TRANSACTION_DATE,
+        ROW_NUMBER()
+        OVER(PARTITION BY USER_ID
+             ORDER BY
+                 TRANSACTION_DATE
+        )
+    FROM
+        USERS_P5
+), USERS ( USER_ID ) AS (
+    SELECT DISTINCT
+        ( USER_ID )
+    FROM
+        USERS_P5
+), SUPERUSERS (
+    USER_ID,
+    DATE_AS_SUPER
+) AS (
+    SELECT
+        USER_ID,
+        TRANSACTION_DATE
+    FROM
+        RANKINGS
+    WHERE
+        RANKED_DATE = 2
+)
 SELECT
-    *
+    U.USER_ID,
+    S.DATE_AS_SUPER
 FROM
-    RANKINGS_P5;
-
-
-CREATE TABLE DIUSERS_P5
-    AS
-        SELECT DISTINCT
-            ( USER_ID )
-        FROM
-            USERS_P5;
-
-
-SELECT
-    *
-FROM
-    DIUSERS_P5;
-
-
-CREATE TABLE SUPERUSERS_P5
-    AS
-        SELECT
-            USER_ID,
-            TRANSACTION_DATE
-        FROM
-            RANKINGS_P5
-        WHERE
-            RANKED_DATE = 2
-
-
-SELECT
-    *
-FROM
-    SUPERUSERS_P5;
-
-
-SELECT
-    D.USER_ID,
-    S.TRANSACTION_DATE
-FROM
-    DIUSERS_P5    D
-    LEFT JOIN SUPERUSERS_P5 S ON D.USER_ID = S.USER_ID;
+    USERS      U
+    LEFT JOIN SUPERUSERS S ON U.USER_ID = S.USER_ID
+ORDER BY
+    2;
