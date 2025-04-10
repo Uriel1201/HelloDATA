268edd57868024701193d2cd3b72cd247c134f02
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-10 02:48:02 -0600

    Create query.py

---
+++ content_recommendation/query.py
@@ -0,0 +1,28 @@
+# pip install pandas
+# pip install numpy
+# pip install SQLAlchemy
+# pip install cx_Oracle
+
+import pandas as pd
+import numpy as np
+import cx_Oracle
+import sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
+
+try:
+    # Cadena de conexión a la base de datos (reemplaza con tus propios detalles)
+    engine = sqlalchemy.create_engine("oracle+cx_oracle://usr:pswd@localhost/?service_name=orclpdb1", arraysize=1000)
+
+    # Consulta SQL para obtener los datos
+    table = """SELECT * FROM USERS_P5;"""
+    
+    # Leer datos en un DataFrame de pandas
+    
+
+except SQLAlchemyError as e:
+    print(f"Error al conectar a la base de datos o al ejecutar la consulta: {e}")
+
+finally:
+    # Opcional: Cerrar la conexión si es necesario, pero SQLAlchemy se encarga de gestionarla generalmente.
+    if 'engine' in locals():
+        engine.dispose()
