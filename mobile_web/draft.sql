92f05fa63f547e10440b23387c3ab50967d2f100
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-10 15:20:51 -0600

    Update draft.sql

--- mobile_web/draft.sql
+++ mobile_web/draft.sql
@@ -2,3 +2,10 @@ select m.uset_id, w.user_id
 from mobile_p7 m
 full outer join web_p7 w
 on m.user_id=w.user_id;
+
+SELECT
+    M.USER_ID AS MO,
+    W.USER_ID AS WE
+FROM
+    MOBILE_P7 M
+    FULL OUTER JOIN WEB_P7    W ON M.USER_ID = W.USER_ID;
