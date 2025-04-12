3f8f0594d23f8d1e83b650a97fcc6807af9083ff
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-12 15:49:21 -0600

    Add files via upload

---
+++ upgrade_date/live-sql-demo-3.sql
@@ -0,0 +1,38 @@
+WITH F2_USERS (
+    USER_ID,
+    JOIN_DATE
+) AS (
+    SELECT
+        E.USER_ID,
+        U.JOIN_DATE
+    FROM
+             EVENTS_P8 E
+        INNER JOIN USERS_P8 U ON E.USER_ID = U.USER_ID
+    WHERE
+        E.TYPE = 'F2'
+), P_USERS (
+    USER_ID,
+    ACCESS_DATE
+) AS (
+    SELECT
+        USER_ID,
+        ACCESS_DATE
+    FROM
+        EVENTS_P8
+    WHERE
+        TYPE = 'P'
+)
+SELECT
+    ROUND(
+        AVG(
+            CASE
+                WHEN P.ACCESS_DATE - F.JOIN_DATE <= 30 THEN
+                    1
+                ELSE 0
+            END
+        ),
+        2
+    ) AS UPGRADE_RATE
+FROM
+    F2_USERS F
+    LEFT JOIN P_USERS  P ON F.USER_ID = P.USER_ID;
