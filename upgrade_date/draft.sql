c6c8479e59c6a607aec551458ced2e32f78de5b2
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-12 13:06:05 -0600

    Update draft.sql

--- upgrade_date/draft.sql
+++ upgrade_date/draft.sql
@@ -1,8 +1,12 @@
-SELECT
+with F2_USERS(
+    user_id,
+    join_date
+) as (SELECT
     E.USER_ID,
     U.JOIN_DATE
 FROM
          EVENTS_P8 E
     INNER JOIN USERS_P8 U ON E.USER_ID = U.USER_ID
 WHERE
-    E.TYPE = 'F2';
+    E.TYPE = 'F2')
+
