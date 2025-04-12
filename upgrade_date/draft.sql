71761348ee0aec7d9343eb3242e7a6a9eec17187
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-12 13:51:53 -0600

    Update draft.sql

--- upgrade_date/draft.sql
+++ upgrade_date/draft.sql
@@ -22,15 +22,16 @@ WITH F2_USERS (
         INNER JOIN USERS_P8 U ON E.USER_ID = U.USER_ID
     WHERE
         E.TYPE = 'F2'
-), P_USERS (
-    USER_ID,
-    JOIN_DATE,
-    ACCESS_DATE
-) AS (
-    SELECT
+), p_users(
+    user_id,
+    Access_date
+) as (
+    select user_id, access_date
+    from events_p8
+    where type='P'
+)  SELECT
         F.USER_ID,
-        F.JOIN_DATE,
-        E.ACCESS_DATE
+        E.ACCESS_DATE-f.join_date
     FROM
         F2_USERS  F
         LEFT JOIN EVENTS_P8 E ON F.USER_ID = E.USER_ID
