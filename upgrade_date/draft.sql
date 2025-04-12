48c98113cbf7b7534816b3791963fc13670107fb
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-12 13:32:29 -0600

    Update draft.sql

--- upgrade_date/draft.sql
+++ upgrade_date/draft.sql
@@ -41,3 +41,33 @@ SELECT
     *
 FROM
     P_USERS;
+/*-----------------------*/
+WITH F2_USERS (
+    USER_ID,
+    JOIN_DATE
+) AS (
+    SELECT
+        E.USER_ID,
+        U.JOIN_DATE
+    FROM
+             EVENTS_P8 E
+        INNER JOIN USERS_P8 U ON E.USER_ID = U.USER_ID
+    WHERE
+        E.TYPE = 'F2'
+), P_UPGRADE (
+    USER_ID,
+    UPGRADE_TIME
+) AS (
+    SELECT
+        F.USER_ID,
+        E.ACCESS_DATE - F.JOIN_DATE
+    FROM
+        F2_USERS  F
+        LEFT JOIN EVENTS_P8 E ON F.USER_ID = E.USER_ID
+    WHERE
+        E.TYPE = 'P'
+)
+SELECT
+    *
+FROM
+    P_UPGRADE;
