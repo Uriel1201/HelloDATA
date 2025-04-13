90f5f610780d51c748ca9825a4d081faf543b2f3
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-13 17:57:42 -0600

    Update draft.sql

--- project_aggregation/draft.sql
+++ project_aggregation/draft.sql
@@ -1,30 +1,31 @@
-with beginning(
-  start_project
-) as (
+WITH BEGINNING ( START_PROJECT ) AS (
+    SELECT
+        START_DATE
+    FROM
+        PROJECTS_P10
+    WHERE
+        START_DATE NOT IN (
+            SELECT
+                END_DATE
+            FROM
+                PROJECTS_P10
+        )
+), COMPLETION ( END_PROJECT ) AS (
+    SELECT
+        END_DATE
+    FROM
+        PROJECTS_P10
+    WHERE
+        END_DATE NOT IN (
+            SELECT
+                START_DATE
+            FROM
+                PROJECTS_P10
+        )
+)
 SELECT
-    START_DATE
+    START_PROJECT,
+    END_PROJECT
 FROM
-    PROJECTS_P10
-WHERE
-    START_DATE NOT IN (
-        SELECT
-            END_DATE
-        FROM
-            PROJECTS_P10
-    )
-  ), completion (
-  end_project
-) as (
-SELECT
-    end_date
-FROM
-    PROJECTS_P10
-WHERE
-    end_date NOT IN (
-        SELECT
-            start_DATE
-        FROM
-            PROJECTS_P10
-    )
-  ) select start_project, end_project
-           from beginning, completion;
+    BEGINNING,
+    COMPLETION;
