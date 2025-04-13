7417c23beb26c073e0ac3de88cc128448d7df46a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-13 15:04:45 -0600

    Add files via upload

---
+++ most_friended/live-sql-demo-7.sql
@@ -0,0 +1,36 @@
+/* 
+09. Most Friended.
+
+Returning a list of
+users and their corresponding friend
+count. Assuming that only
+unique friendships are displayed. */
+
+/* Querying original data.*/
+SELECT
+    *
+FROM
+    FRIENDS_P9;
+
+/* Querying the number of friends by each user.*/
+WITH FRIENDSHIP ( FRIEND_ID ) AS ( -- number of repeats by each user
+    SELECT
+        USER_1
+    FROM
+        FRIENDS_P9
+    UNION ALL
+    SELECT
+        USER_2
+    FROM
+        FRIENDS_P9
+)
+SELECT
+    FRIEND_ID,
+    COUNT(*) AS NUMBER_OF_FRIENDS
+FROM
+    FRIENDSHIP
+GROUP BY
+    FRIEND_ID
+ORDER BY
+    2 DESC,
+    1;
