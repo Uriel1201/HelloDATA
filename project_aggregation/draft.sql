c67c2828ec89494899619ef3d24c89c52df0020a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-13 18:30:15 -0600

    Create draft.sql

---
+++ project_aggregation/draft.sql
@@ -0,0 +1,47 @@
+WITH BEGINNING ( START_PROJECT ) AS (
+    SELECT
+        START_DATE
+    FROM
+        PROJECTS_P10
+    WHERE
+        START_DATE NOT IN (
+            SELECT
+                END_DATE
+            FROM
+                PROJECTS_P10
+        )
+), COMPLETION ( END_PROJECT ) AS (
+    SELECT
+        END_DATE
+    FROM
+        PROJECTS_P10
+    WHERE
+        END_DATE NOT IN (
+            SELECT
+                START_DATE
+            FROM
+                PROJECTS_P10
+        )
+), TIMELINE (
+    START_PROJECT,
+    END_PROJECT
+) AS (
+    SELECT
+        START_PROJECT,
+        MIN(END_PROJECT)
+    FROM
+        BEGINNING,
+        COMPLETION
+    WHERE
+        START_PROJECT < END_PROJECT
+    GROUP BY
+        START_PROJECT
+)
+SELECT
+    START_PROJECT,
+    END_PROJECT,
+    END_PROJECT - START_PROJECT AS PROJECT_DURATION
+FROM
+    TIMELINE
+ORDER BY
+    3;
