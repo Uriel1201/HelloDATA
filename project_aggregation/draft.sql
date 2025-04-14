60087905c2c9a3bc061bcaab3a7e292bd7eee220
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-13 18:27:24 -0600

    Update draft.sql

--- project_aggregation/draft.sql
+++ project_aggregation/draft.sql
@@ -1,39 +1,3 @@
-WITH BEGINNING ( START_PROJECT ) AS (
-    SELECT
-        START_DATE
-    FROM
-        PROJECTS_P10
-    WHERE
-        START_DATE NOT IN (
-            SELECT
-                END_DATE
-            FROM
-                PROJECTS_P10
-        )
-), COMPLETION ( END_PROJECT ) AS (
-    SELECT
-        END_DATE
-    FROM
-        PROJECTS_P10
-    WHERE
-        END_DATE NOT IN (
-            SELECT
-                START_DATE
-            FROM
-                PROJECTS_P10
-        )
-) timeline (
-    start_project,
-    end_project
-)
-SELECT
-    START_PROJECT,
-    min(END_PROJECT)
-FROM
-    BEGINNING,
-    COMPLETION
-where start_project < end_project
-group by start_project;
 /************************************/
 WITH BEGINNING ( START_PROJECT ) AS (
     SELECT
