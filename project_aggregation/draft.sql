353c836ee348d9501d2903d0afc3bc0cd6c7357b
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-13 18:26:09 -0600

    Update draft.sql

--- project_aggregation/draft.sql
+++ project_aggregation/draft.sql
@@ -34,3 +34,44 @@ FROM
     COMPLETION
 where start_project < end_project
 group by start_project;
+/************************************/
+WITH BEGINNING ( START_PROJECT ) AS (
+    SELECT
+        START_DATE
+    FROM
+        PROJECTS_P10
+    WHERE
+        START_DATE NOT IN (
+            SELECT
+                END_DATE
+            FROM
+                PROJECTS_P10
+        )
+), COMPLETION ( END_PROJECT ) AS (
+    SELECT
+        END_DATE
+    FROM
+        PROJECTS_P10
+    WHERE
+        END_DATE NOT IN (
+            SELECT
+                START_DATE
+            FROM
+                PROJECTS_P10
+        )
+), timeline (
+    start_project,
+    end_project
+) as 
+(SELECT
+    START_PROJECT,
+    MIN(END_PROJECT)
+FROM
+    BEGINNING,
+    COMPLETION
+WHERE
+    START_PROJECT < END_PROJECT
+GROUP BY
+    START_PROJECT
+) select start_project, end_project, end_project-start_project as project_duration
+  from timeline;
