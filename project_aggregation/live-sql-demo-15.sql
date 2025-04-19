67d9b31a63ef26040d3ef9989af2f509a1d10850
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-18 20:52:37 -0600

    Add files via upload

---
+++ project_aggregation/live-sql-demo-15.sql
@@ -0,0 +1,35 @@
+WITH SORTED ( -- ordering dates
+    S_D,
+    E_D,
+    PREV_END
+) AS (
+    SELECT
+        START_DATE,
+        END_DATE,
+        LAG(END_DATE)
+        OVER(
+            ORDER BY
+                START_DATE
+        )
+    FROM
+        PROJECTS_P10
+)
+SELECT
+    S_D,
+    E_D,
+    SUM(
+        CASE
+            WHEN PREV_END IS NULL THEN
+                1
+            WHEN PREV_END = S_D THEN
+                0
+            ELSE
+                1
+        END
+    )
+FROM
+    SORTED
+GROUP BY
+    S_D,
+    E_D,
+    PREV_END;
