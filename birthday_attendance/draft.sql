31f70e83b1c12d14384ef6a1833892d16661301c
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-19 18:02:45 -0600

    Delete birthday_attendance/draft.sql

--- birthday_attendance/draft.sql
+++
@@ -1,6 +0,0 @@
-select s.student_id, a.attendance
-from students s
-left join attendance a
-on s.student_id=a.student_id
-and extract(month from a.school_date)=extract(month from s.date_of_birth)
-and extract(day from a.school_date)=extract(day from s.date_of_birth);
