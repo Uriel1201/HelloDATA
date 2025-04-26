24152952687bfbf8e234697c4973c95be12dad45
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-26 12:25:33 -0600

    Add files via upload

---
+++ 03_frequent_item/live-sql-demo-6.sql
@@ -0,0 +1,50 @@
+/*
+03. Most Frequent Item
+
+Writing a query to return the most frequent item
+ordered on each date. */
+
+/*Querying original data*/
+SELECT
+    *
+FROM
+    ITEMS_P3;
+
+        /*Querying most frequent items on each date*/
+WITH FREQUENCIES ( --Frequencies for items on each date
+    DATES,
+    ITEM,
+    FREQUENCY
+) AS (
+    SELECT
+        DATES,
+        ITEM,
+        COUNT(*)
+    FROM
+        ITEMS_P3
+    GROUP BY
+        DATES,
+        ITEM
+), ITEMS_RANKING ( --ranking items on each date
+    DATES,
+    ITEM,
+    RANKING
+) AS (
+    SELECT
+        DATES,
+        ITEM,
+        RANK()
+        OVER(PARTITION BY DATES
+             ORDER BY
+                 FREQUENCY DESC
+        )
+    FROM
+        FREQUENCIES
+)
+SELECT
+    DATES,
+    ITEM
+FROM
+    ITEMS_RANKING
+WHERE
+    RANKING = 1;
