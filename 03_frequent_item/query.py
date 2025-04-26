52e26b5612d38684a29a63c056e3868b61e96eab
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-26 16:12:08 -0600

    Update query.py

--- 03_frequent_item/query.py
+++ 03_frequent_item/query.py
@@ -10,6 +10,21 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     items=pl.from_arrow(pyarrow_table)
-                    
+    polars_counts=(polars_items.group_by(['dates','item'])
+                           .agg(pl.len()
+                                  .alias('count')
+                            )
+                           .with_columns(pl.max('count')
+                                           .over(partition_by='dates')
+                                           .alias('max_count')
+                            )
+                           .filter(pl.col('count')==pl.col('max_count')
+                            )
+                           .select(pl.col('dates'),
+                                   pl.col('item')
+                            )
+)
+print(f'item most frequented for each date using Polars:')
+polars_counts             
 finally:
     conn.close()
