03cfd06a0128325ad28aad9726f88fc6942957a2
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-26 06:44:31 -0600

    Update query.py

--- 01_cancellation_rates/query.py
+++ 01_cancellation_rates/query.py
@@ -10,6 +10,17 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     users=pl.from_arrow(pyarrow_table)
+    rates=(users.to_dummies(columns='action')
+                .drop('dates')
+                .group_by('user_id')
+                .agg(pl.col('*').sum())
+                .select(pl.col('user_id'),
+                        pl.col('action_publish')/pl.col('action_start'),
+                                  pl.col('action_cancel')/pl.col('action_start')
+                           )
+)
+print(f'Rates for each user using Polars:')
+polars_rates
                           
 finally:
     conn.close()
