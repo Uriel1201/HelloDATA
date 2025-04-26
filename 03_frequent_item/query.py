8217fde46745e6415990b4f534f13668c5c85c20
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-26 16:11:03 -0600

    Update query.py

--- 03_frequent_item/query.py
+++ 03_frequent_item/query.py
@@ -1,1 +1,15 @@
+# python -m pip install oracledb numpy pyarrow polars --upgrade
+import numpy as np
+import polars as pl
+import oracledb
+import pyarrow
 
+try:
+    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
+    table="SELECT * FROM ITEMS_P3"
+    odf=conn.fetch_df_all(statement=table,arraysize=100)
+    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
+    items=pl.from_arrow(pyarrow_table)
+                    
+finally:
+    conn.close()
