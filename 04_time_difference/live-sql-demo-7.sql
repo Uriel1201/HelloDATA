2c2c98b6a56e1c87b3ce005cdfe2cc768f9cd9e5
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-27 11:57:26 -0600

    Add files via upload

---
+++ 04_time_difference/live-sql-demo-7.sql
@@ -0,0 +1,76 @@
+/* 
+04. Time Difference Between Latest Actions
+
+writing a query to return for each user the
+time elapsed between the last action
+and the second-to-last action, in
+ascending order by user ID. */
+
+CREATE TABLE USERS_P4 (
+    ID          INTEGER,
+    ACTIONS     VARCHAR(10),
+    ACTION_DATE DATE
+);
+
+INSERT INTO USERS_P4
+    WITH NAMES AS (
+        SELECT
+            1,
+            'Start',
+            '13-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'Cancel',
+            '13-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            2,
+            'Start',
+            '11-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            2,
+            'Publish',
+            '14-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'Start',
+            '15-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            3,
+            'Cancel',
+            '15-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            4,
+            'Start',
+            '18-feb-20'
+        FROM
+            DUAL
+        UNION ALL
+        SELECT
+            1,
+            'Publish',
+            '19-feb-20'
+        FROM
+            DUAL
+    )
+    SELECT
+        *
+    FROM
+        NAMES;
---
+++ 04_time_difference/live-sql-demo-8.sql
@@ -0,0 +1,62 @@
+/* 
+04. Time Difference Between Latest Actions
+
+writing a query to return for each user the
+time elapsed between the last action
+and the second-to-last action, in
+ascending order by user ID. */ 
+
+/* Querying original data*/
+SELECT
+    *
+FROM
+    USERS_P4;
+
+                /* Time elapsed between the last action 
+                        and the second-to-last action */
+WITH RANKINGS ( -- Ordering dates in descending order grouped by each user
+    ID,
+    ACTION_DATE,
+    RANKED_DATES
+) AS (
+    SELECT
+        ID,
+        ACTION_DATE,
+        ROW_NUMBER()
+        OVER(PARTITION BY ID
+             ORDER BY
+                 ACTION_DATE DESC
+        )
+    FROM
+        USERS_P4
+), RANKED1 ( -- last date recorded for each user
+    ID,
+    LAST
+) AS (
+    SELECT
+        ID,
+        ACTION_DATE
+    FROM
+        RANKINGS
+    WHERE
+        RANKED_DATES = 1
+), RANKED2 ( -- penultimate date recorded for each user
+    ID,
+    PENULTIMATE
+) AS (
+    SELECT
+        ID,
+        ACTION_DATE
+    FROM
+        RANKINGS
+    WHERE
+        RANKED_DATES = 2
+)
+SELECT
+    A.ID,
+    ( A.LAST - B.PENULTIMATE ) AS ELAPSED_TIME
+FROM
+    RANKED1 A
+    LEFT JOIN RANKED2 B ON A.ID = B.ID
+ORDER BY
+    A.ID;
