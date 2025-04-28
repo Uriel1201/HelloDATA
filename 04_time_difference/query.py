2bed6d11fd65ef9c2a327425047f2d138505b2ba
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-28 08:58:07 -0600

    Update query.py

--- 04_time_difference/query.py
+++ 04_time_difference/query.py
@@ -6,24 +6,11 @@ import pyarrow
 
 try:
     conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table="SELECT * FROM ITEMS_P3"
+    table="SELECT * FROM USERS_P4"
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
-    items=pl.from_arrow(pyarrow_table)
-    polars_counts=(items.group_by(['DATES','ITEM'])
-                        .agg(pl.len()
-                               .alias('COUNT')
-                         )
-                        .with_columns(pl.max('COUNT')
-                                        .over(partition_by='DATES')
-                                        .alias('MAX_COUNT')
-                         )
-                        .filter(pl.col('COUNT')==pl.col('MAX_COUNT')
-                         )
-                        .select(pl.col('DATES'),
-                                pl.col('ITEM')
-                         )
-    )
+    users=pl.from_arrow(pyarrow_table)
+    
     print(f'item most frequented for each date using Polars:\n{polars_counts}')          
 finally:
     conn.close()
