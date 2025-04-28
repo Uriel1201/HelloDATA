fc396c7eca6a84010a148c3a39776f959b278a8e
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-28 09:19:14 -0600

    Update query.py

--- 04_time_difference/query.py
+++ 04_time_difference/query.py
@@ -10,19 +10,19 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     users=pl.from_arrow(pyarrow_table)
-    durations=(polars_users.sort(by=['User_id','Action_date']
+    durations=(polars_users.sort(by=['ID','ACTION_DATE']
                                      ,descending=[False,True]
                             )
-                           .with_columns(Elapsed_time=pl.col('Action_date')
+                           .with_columns(ELAPSED_TIME=pl.col('ACTION_DATE')
                                                         .diff(-1)
                                                         .dt
                                                         .total_days()
-                                                        .over(partition_by='User_id')
+                                                        .over(partition_by='ID')
                             )
-                           .group_by('User_id')
+                           .group_by('ID')
                            .first()
-                           .select(pl.col('User_id'),
-                                   pl.col('Elapsed_time')
+                           .select(pl.col('ID'),
+                                   pl.col('ELAPSED_TIME')
                             )
     )
     print(f'Returning time elapsed using Polars:\n{durations}')          
