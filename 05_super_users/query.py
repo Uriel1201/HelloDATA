e73659db346b0ba980de32b0fadb753e8e2023bd
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-29 18:51:07 -0600

    Update query.py

--- 05_super_users/query.py
+++ 05_super_users/query.py
@@ -1,1 +1,15 @@
+# python -m pip install oracledb numpy pyarrow polars --upgrade
+import numpy as np
+import polars as pl
+import oracledb
+import pyarrow
 
+try:
+    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
+    table="SELECT * FROM USERS_P5"
+    odf=conn.fetch_df_all(statement=table,arraysize=100)
+    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
+    users=pl.from_arrow(pyarrow_table)
+    print(f'Returning time elapsed using Polars:\n{durations}')          
+finally:
+    conn.close()
