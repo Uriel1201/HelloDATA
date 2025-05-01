9789a99eeeb98c45d7a9515959c5a90fd409b670
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-30 19:55:58 -0600

    Update query.py

--- 06_content_recomm/query.py
+++ 06_content_recomm/query.py
@@ -6,10 +6,14 @@ import pyarrow
 
 try:
     conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table="SELECT * FROM USERS_P5"
-    odf=conn.fetch_df_all(statement=table,arraysize=100)
-    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
-    users=pl.from_arrow(pyarrow_table)
+    table1="SELECT * FROM FRIENDS_P6"
+    table2="SELECT * FROM LIKES_P6"
+    odf1=conn.fetch_df_all(statement=table1,arraysize=100)
+    odf2=conn.fetch_df_all(statement=table2,arraysize=100)
+    pyarrow_table1=pyarrow.Table.from_arrays(odf1.column_arrays(),names=odf1.column_names())
+    pyarrow_table2=pyarrow.Table.from_arrays(odf2.column_arrays(),names=odf2.column_names())
+    friends=pl.from_arrow(pyarrow_table1).lazy()
+    likes=pl.from_arrow(pyarrow_table2).lazy()
   
     print(f'Convertion date to superusers using Polars:\n{superusers}')          
 finally:
