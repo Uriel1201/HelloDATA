80ed2bd4408a74038954ba129a7095e5882953d0
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-04-30 19:02:47 -0600

    Update query.py

--- 06_content_recomm/query.py
+++ 06_content_recomm/query.py
@@ -1,1 +1,16 @@
+# python -m pip install oracledb numpy pyarrow polars --upgrade
+import numpy as np
+import polars as pl
+import oracledb
+import pyarrow
 
+try:
+    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
+    table="SELECT * FROM USERS_P5"
+    odf=conn.fetch_df_all(statement=table,arraysize=100)
+    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
+    users=pl.from_arrow(pyarrow_table)
+  
+    print(f'Convertion date to superusers using Polars:\n{superusers}')          
+finally:
+    conn.close()
