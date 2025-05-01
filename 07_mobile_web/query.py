7e3ac2a9cf3eb1322536700e58186b76aa636f72
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-01 15:19:38 -0600

    Update query.py

--- 07_mobile_web/query.py
+++ 07_mobile_web/query.py
@@ -1,1 +1,20 @@
+# python -m pip install oracledb numpy pyarrow polars --upgrade
+import numpy as np
+import polars as pl
+import oracledb
+import pyarrow
 
+try:
+    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
+    table1="SELECT * FROM MOBILE_P7"
+    table2="SELECT * FROM WEB_P7"
+    odf1=conn.fetch_df_all(statement=table1,arraysize=100)
+    odf2=conn.fetch_df_all(statement=table2,arraysize=100)
+    pyarrow_table1=pyarrow.Table.from_arrays(odf1.column_arrays(),names=odf1.column_names())
+    pyarrow_table2=pyarrow.Table.from_arrays(odf2.column_arrays(),names=odf2.column_names())
+    mobile=pl.from_arrow(pyarrow_table1).lazy()
+    web=pl.from_arrow(pyarrow_table2).lazy()
+  
+    print(f'Recommendations:\n{recommendations}')          
+finally:
+    conn.close()
