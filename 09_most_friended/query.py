b2b559cdc2a1252f0f1f1062edab14f6efc59480
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-03 14:49:03 -0600

    Update query.py

--- 09_most_friended/query.py
+++ 09_most_friended/query.py
@@ -10,6 +10,21 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     friends=pl.from_arrow(pyarrow_table).lazy()
+    friendship=(pl.sql("""
+                       SELECT user_1 as user_id FROM pl_friends
+                       UNION ALL
+                       SELECT user_2 as user_id FROM pl_friends
+                       """
+                   )
+                  .group_by('*')
+                  .agg(pl.len()
+                         .alias('number_of_friends')
+                   )
+                  .sort(by='number_of_friends',
+                        descending=True
+                   )
+    ).collect()
+    print(f'Returning number of friends by each user:\n{friendship}')
               
 finally:
     conn.close()
