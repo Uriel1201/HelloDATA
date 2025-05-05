3b8918d201bbeeca77677f1f4fef2bd8ca83faa3
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-05 15:55:43 -0600

    Update query.py

--- 10_project_agg/query.py
+++ 10_project_agg/query.py
@@ -6,24 +6,11 @@ import pyarrow
 
 try:
     conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table="SELECT * FROM FRIENDS_P9"
+    table="SELECT * FROM PROJECTS_P10"
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
-    friends=pl.from_arrow(pyarrow_table).lazy()
-    friendship=(pl.sql("""
-                       SELECT USER_1 as USER_ID FROM friends
-                       UNION ALL
-                       SELECT USER_2 as USER_ID FROM friends
-                       """
-                   )
-                  .group_by('*')
-                  .agg(pl.len()
-                         .alias('NUMBER_OF_FRIENDS')
-                   )
-                  .sort(by='NUMBER_OF_FRIENDS',
-                        descending=True
-                   )
-    ).collect()
+    projects=pl.from_arrow(pyarrow_table).lazy()
+    
     print(f'Returning number of friends by each user:\n{friendship}')
               
 finally:
