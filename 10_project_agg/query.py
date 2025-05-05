f74e9ba494eca3e4a4f7d94df3987de76a279b8f
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-05 16:02:56 -0600

    Update query.py

--- 10_project_agg/query.py
+++ 10_project_agg/query.py
@@ -10,8 +10,25 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     projects=pl.from_arrow(pyarrow_table).lazy()
-    
-    print(f'Returning number of friends by each user:\n{friendship}')
-              
+    durations=(projects.sort(by='START_DATE')
+                       .with_columns(PROJECT_ID=pl.when(pl.col('START_DATE').eq(pl.col('END_DATE')
+                                                                                  .shift(1)
+                                                                             )
+                                                   )
+                                                  .then(0)
+                                                  .otherwise(1)
+                                                  .cum_sum()
+                        )
+                       .group_by('PROJECT_ID')
+                       .agg(PROJECT_START=pl.first('START_DATE'),
+                            PROJECT_END=pl.last('END_DATE')
+                        )
+                       .with_columns(DURATION=(pl.col('PROJECT_END')-pl.col('PROJECT_START')).dt
+                                                                                             .total_days()
+                        )
+                       .sort(by='DURATION')
+    ).collect()
+    print(f'Duration of each unique project:\n{durations}')
+
 finally:
     conn.close()
