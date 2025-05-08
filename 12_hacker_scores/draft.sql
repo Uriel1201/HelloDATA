4205165002c87f2358b4ba4fe60e0c7faf248dba
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-08 13:40:14 -0600

    Update draft.sql

--- 12_hacker_scores/draft.sql
+++ 12_hacker_scores/draft.sql
@@ -1,19 +1,35 @@
-with total_scores(hacker_id, total) as (
-SELECT
+/***********************************/
+WITH TOTAL_SCORES (
     HACKER_ID,
-    SUM(MAX_SCORE) AS SUMA
+    SCORE
+) AS (
+    SELECT
+        HACKER_ID,
+        SUM(MAX_SCORE)
+    FROM
+        (
+            SELECT
+                HACKER_ID,
+                CHALLENGE_ID,
+                MAX(SCORE) AS MAX_SCORE
+            FROM
+                SUBMISSIONS_P12
+            GROUP BY
+                HACKER_ID,
+                CHALLENGE_ID
+        )
+    GROUP BY
+        HACKER_ID
+)
+SELECT
+    H.HACKER_ID,
+    H.NAME,
+    T.SCORE
 FROM
-    (
-        SELECT
-            HACKER_ID,
-            CHALLENGE_ID,
-            MAX(SCORE) AS MAX_SCORE
-        FROM
-            SUBMISSIONS_P12
-        GROUP BY
-            HACKER_ID,
-            CHALLENGE_ID
-    )
-GROUP BY
-    HACKER_ID)
-
+    HACKERS_P12  H
+    LEFT JOIN TOTAL_SCORES T ON H.HACKER_ID = T.HACKER_ID
+WHERE
+    T.SCORE > 0
+ORDER BY
+    3 DESC,
+    1;
