e39f5d21fea24efeb006ade15b10f7c55ad0150d
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-08 12:45:54 -0600

    Update draft.sql

--- 12_hacker_scores/draft.sql
+++ 12_hacker_scores/draft.sql
@@ -1,1 +1,17 @@
-
+SELECT
+    HACKER_ID,
+    SUM(MAX_SCORE) AS SUMA
+FROM
+    (
+        SELECT
+            HACKER_ID,
+            CHALLENGE_ID,
+            MAX(SCORE) AS MAX_SCORE
+        FROM
+            SUBMISSIONS_P12
+        GROUP BY
+            HACKER_ID,
+            CHALLENGE_ID
+    )
+GROUP BY
+    HACKER_ID;
