c420cc7aaf0cd6bee58c486d4fda9676642ae17e
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-08 18:35:12 -0600

    Update query.py

--- 12_hacker_scores/query.py
+++ 12_hacker_scores/query.py
@@ -6,14 +6,14 @@ import pyarrow
 
 try:
     conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table1="SELECT * FROM ATTENDANCE_P11"
-    table2="SELECT * FROM STUDENTS_P11"
+    table1="SELECT * FROM HACKERS_P12"
+    table2="SELECT * FROM SUBMISSIONS_P12"
     odf1=conn.fetch_df_all(statement=table1,arraysize=100)
     odf2=conn.fetch_df_all(statement=table2,arraysize=100)
     pyarrow_table1=pyarrow.Table.from_arrays(odf1.column_arrays(),names=odf1.column_names())
     pyarrow_table2=pyarrow.Table.from_arrays(odf2.column_arrays(),names=odf2.column_names())
-    attendance=pl.from_arrow(pyarrow_table1).lazy()
-    students=pl.from_arrow(pyarrow_table2).lazy()
+    hackers=pl.from_arrow(pyarrow_table1).lazy()
+    submissions=pl.from_arrow(pyarrow_table2).lazy()
     print(f'{birthday.head(5)}\nfraction of attendances in birthday dates:\n{fraction}')
 
 finally:
