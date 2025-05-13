bc1a16c137bed4d042408526a417e5f93826ae65
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-13 09:27:52 -0600

    Create query.py

---
+++ 14_cummulative/query.py
@@ -0,0 +1,15 @@
+# python -m pip install oracledb numpy pyarrow polars --upgrade
+import numpy as np
+import polars as pl
+import oracledb
+import pyarrow
+
+try:
+    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
+    table="SELECT * FROM PROJECTS_P10"
+    odf=conn.fetch_df_all(statement=table,arraysize=100)
+    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
+    projects=pl.from_arrow(pyarrow_table).lazy()
+
+finally:
+    conn.close()
