daf41e2c23c6dc0011a08086d6f64adecb9aa03b
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-13 09:34:42 -0600

    Update query.py

--- 14_cummulative/query.py
+++ 14_cummulative/query.py
@@ -10,6 +10,23 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     employee=pl.from_arrow(pyarrow_table).lazy()
-
+    cumulative=(employee.sort(by=['ID','PAY_MONTH'])
+                        .with_columns(CUMULATIVE_PAY=pl.col('SALARY')
+                                                       .cum_sum()
+                                                       .over('ID'),
+                                      RANK=pl.col('PAY_MONTH')
+                                             .rank(method='dense',
+                                                   descending=True
+                                              )
+                                             .over('ID')
+                         )
+                        .filter(pl.col('RANK')>1)
+                        .select(pl.col('ID'),
+                                pl.col('PAY_MONTH'),
+                                pl.col('CUMULATIVE_PAY')
+                         )
+    )
+    print(f'Returning the cumulative pays excluding the last month:\n {cumulative.collect()}')
+     
 finally:
     conn.close()
