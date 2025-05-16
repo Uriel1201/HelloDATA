ad83a4f587e97d266fa363a1591f2534935918b7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-16 16:34:08 -0600

    Update query.py

--- 15_match_standings/query.py
+++ 15_match_standings/query.py
@@ -1,1 +1,19 @@
+# python -m pip install oracledb numpy pyarrow polars --upgrade
+import numpy as np
+import polars as pl
+import oracledb
+import pyarrow
 
+try:
+    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
+    table1="SELECT * FROM ATTENDANCE_P11"
+    table2="SELECT * FROM STUDENTS_P11"
+    odf1=conn.fetch_df_all(statement=table1,arraysize=100)
+    odf2=conn.fetch_df_all(statement=table2,arraysize=100)
+    pyarrow_table1=pyarrow.Table.from_arrays(odf1.column_arrays(),names=odf1.column_names())
+    pyarrow_table2=pyarrow.Table.from_arrays(odf2.column_arrays(),names=odf2.column_names())
+    attendance=pl.from_arrow(pyarrow_table1).lazy()
+    students=pl.from_arrow(pyarrow_table2).lazy()
+    
+finally:
+    conn.close()
