85afc2db2b55d214389c49903966a8830773c2cb
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-16 16:38:07 -0600

    Update query.py

--- 15_match_standings/query.py
+++ 15_match_standings/query.py
@@ -6,14 +6,14 @@ import pyarrow
 
 try:
     conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table1="SELECT * FROM ATTENDANCE_P11"
-    table2="SELECT * FROM STUDENTS_P11"
+    table1="SELECT * FROM TEAMS_P15"
+    table2="SELECT * FROM MATCHES_P15"
     odf1=conn.fetch_df_all(statement=table1,arraysize=100)
     odf2=conn.fetch_df_all(statement=table2,arraysize=100)
     pyarrow_table1=pyarrow.Table.from_arrays(odf1.column_arrays(),names=odf1.column_names())
     pyarrow_table2=pyarrow.Table.from_arrays(odf2.column_arrays(),names=odf2.column_names())
-    attendance=pl.from_arrow(pyarrow_table1).lazy()
-    students=pl.from_arrow(pyarrow_table2).lazy()
+    teams=pl.from_arrow(pyarrow_table1).lazy()
+    matches=pl.from_arrow(pyarrow_table2).lazy()
     
 finally:
     conn.close()
