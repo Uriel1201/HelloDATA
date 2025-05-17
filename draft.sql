e01c6901a3a1daa911ea1472e5f3d7be9bcbdcf5
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-17 17:04:40 -0600

    Update draft.sql

--- draft.sql
+++ draft.sql
@@ -1,4 +1,4 @@
-SELECT NAME || '(' || SUBSTR(OCCUPATION,1,1) || ')' FROM OCCUPATIONS; SELECT 'There are a total of' || COUNT(*) || OCCUPATION || 's.' FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY COUNT(*), OCCUPATION;
+SELECT NAME || '(' || SUBSTR(OCCUPATION,1,1) || ')' FROM OCCUPATIONS; SELECT 'There are a total of' || PROF_C || OCCUPATION || 's.' FROM (SELECT OCCUPATION, COUNT(*) AS PROF_C FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY 2, 1);
 /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
