8ba26c268099140426840ea55fe91fb951393e2a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-19 21:02:36 -0600

    Delete draft.sql

--- draft.sql
+++
@@ -1,5 +0,0 @@
-SELECT NAME || '(' || SUBSTR(OCCUPATION,1,1) || ')' FROM OCCUPATIONS; SELECT 'There are a total of' || PROF_C || OCCUPATION || 's.' FROM (SELECT OCCUPATION, COUNT(*) AS PROF_C FROM OCCUPATIONS GROUP BY OCCUPATION ORDER BY 2, 1);
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
