088c66f68ae429b06da54964874c0840725d4f0d
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-21 14:25:02 -0600

    Update query.jl

--- 02_changes_net_worth/query.jl
+++ 02_changes_net_worth/query.jl
@@ -1,6 +1,3 @@
-url = "https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/02_changes_net_worth/data.tsv";
-download(url, "transactions.tsv")
-
 begin
     using Pkg
     Pkg.add(["CSV", "DataFrames"])
@@ -8,6 +5,46 @@ begin
     using DataFrames
 end
 
+url = "https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/02_changes_net_worth/data.tsv";
+download(url, "transactions.tsv")
 transactions = CSV.read("transactions.tsv", DataFrame; delim = '\t')
+
 sample = first(transactions, 5)
-println("transactions table SAMPLE:\n$sample")
+println("transactions table (SAMPLE):\n$sample")
+
+type_ = (rename!(stack(select(sample,
+                              :AMOUNT,
+                              :SENDER,
+                              :RECEIVER
+                             ),
+                       2:3
+                      ),
+                 :variable => :TYPE,
+                 :value => :USER_ID
+                )
+)
+transform!(type_, [:TYPE, :AMOUNT] => ByRow((x, y) -> x == "SENDER" ? -1 * y : y) => :AMOUNT)
+println("\nType of transaction made by each user (SAMPLE):\n$type_")
+
+df = (rename!(stack(select(transactions,
+                           :AMOUNT,
+                           :SENDER,
+                           :RECEIVER
+                          ),
+                    2:3
+                   ),
+              :variable => :TYPE,
+              :value => :USER_ID
+             )
+)
+transform!(df, [:TYPE, :AMOUNT] => ByRow((x, y) -> x == "SENDER" ? -1 * y : y) => :AMOUNT)
+result = (sort(combine(groupby(df,
+                               :USER_ID
+                              ),
+                       :AMOUNT => sum    
+                      ),
+               :AMOUNT_sum,
+               rev = true
+              )
+)
+println("\nNet changes:\n$result")
