5e66d11bc91ff9cceea97af8106789b44268c4f8
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-21 09:47:06 -0600

    Update query.py

--- 01_cancellation_rates/query.py
+++ 01_cancellation_rates/query.py
@@ -6,32 +6,32 @@
 
 try:
     # You need to write your credentials in the following connection.
-    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table="SELECT * FROM USERS_P1"
-    odf=conn.fetch_df_all(statement=table,arraysize=100)
-    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
-    users=pl.from_arrow(pyarrow_table)
+    conn = oracledb.connect(user = "[Username]", password = "[Password]", dsn = "localhost:1521/FREEPDB1")
+    table = "SELECT * FROM USERS_P1"
+    odf = conn.fetch_df_all(statement = table, arraysize = 100)
+    pyarrow_table = pyarrow.Table.from_arrays(odf.column_arrays(), names = odf.column_names())
+    users = pl.from_arrow(pyarrow_table)
     
     '''
     Alternative 2: Querying directly from this repository 
-    url="https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/01_cancellation_rates/data.tsv"
-    users=pl.scan_csv(url,
-                      separator="\t",
-                      has_header=True,
-                      infer_schema_length=1000,
-                      ignore_errors=False
+    url = "https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/01_cancellation_rates/data.tsv"
+    users = pl.scan_csv(url,
+                        separator="\t",
+                        has_header=True,
+                        infer_schema_length=1000,
+                        ignore_errors=False
     )
-    users=users.collect()
+    users = users.collect()
     '''
     print(f'users table SAMPLE(5):\n{users.head(5)')
-    rates=(users.to_dummies(columns='ACTION')
-                .drop('DATES')
-                .group_by('USER_ID')
-                .agg(pl.col('*').sum())
-                .select(pl.col('USER_ID'),
-                        publish_rate=pl.col('ACTION_publish')/pl.col('ACTION_start'),
-                        cancel_rate=pl.col('ACTION_cancel')/pl.col('ACTION_start')
-                 )
+    rates = (users.to_dummies(columns = 'ACTION')
+                  .drop('DATES')
+                  .group_by('USER_ID')
+                  .agg(pl.col('*').sum())
+                  .select(pl.col('USER_ID'),
+                          publish_rate = pl.col('ACTION_publish') / pl.col('ACTION_start'),
+                          cancel_rate = pl.col('ACTION_cancel') / pl.col('ACTION_start')
+                   )
     )
     print(f'Rates for each user using Polars:\n{rates}')                 
 
