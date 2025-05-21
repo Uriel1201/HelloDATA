926e27d503c076f366d248d431cc8eadb74d8487
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-20 18:19:05 -0600

    Update query.py

--- 01_cancellation_rates/query.py
+++ 01_cancellation_rates/query.py
@@ -10,6 +10,19 @@ try:
     odf=conn.fetch_df_all(statement=table,arraysize=100)
     pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
     users=pl.from_arrow(pyarrow_table)
+    
+    '''
+    Alternative 2: Querying directly from this repository 
+    url="https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/01_cancellation_rates/data.tsv"
+    users=pl.scan_csv(url,
+                      separator="\t",
+                      has_header=True,
+                      infer_schema_length=1000,
+                      ignore_errors=False
+    )
+    users=users.collect()
+    '''
+    
     rates=(users.to_dummies(columns='ACTION')
                 .drop('DATES')
                 .group_by('USER_ID')
@@ -19,7 +32,7 @@ try:
                         cancel_rate=pl.col('ACTION_cancel')/pl.col('ACTION_start')
                  )
     )
-    print(f'Rates for each user using Polars:{rates}')                 
+    print(f'Rates for each user using Polars:\n{rates}')                 
 
 finally:
     conn.close()
