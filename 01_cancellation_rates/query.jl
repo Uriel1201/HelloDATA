38ebe4b4d43093da30be68cd7b9357d571341384
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-21 10:15:07 -0600

    Update query.jl

--- 01_cancellation_rates/query.jl
+++ 01_cancellation_rates/query.jl
@@ -1,1 +1,24 @@
+url = "https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/01_cancellation_rates/data.tsv";
+download(url, "users.tsv")
 
+begin
+    using Pkg
+    Pkg.add(["CSV", "DataFrames"])
+    using CSV
+    using DataFrames
+end
+
+users = CSV.read("users.tsv", DataFrame; delim = '\t')
+println("$users")
+dummy = select(users, :USER_ID, [:ACTION => ByRow(isequal(v)) => Symbol(v) for v in unique(users.ACTION)])
+println("\n$dummy")
+totals = combine(groupby(dummy, :USER_ID), names(dummy, Not(:USER_ID)) .=> sum)
+totals.cancel_rate = @.ifelse(totals.start_sum != 0, totals.cancel_sum ./ totals.start_sum, 0.0)
+totals.publish_rate = @.ifelse(totals.start_sum != 0, totals.publish_sum ./ totals.start_sum, 0.0)
+result = select(
+    totals,
+    :USER_ID,
+    :cancel_rate,
+    :publish_rate
+)
+println("\n$result")
