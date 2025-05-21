fe5c9cb3a56665aa4dde90dba0515be8a40adb82
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-21 11:53:56 -0600

    Update query.jl

--- 02_changes_net_worth/query.jl
+++ 02_changes_net_worth/query.jl
@@ -1,1 +1,12 @@
+url = "https://raw.githubusercontent.com/Uriel1201/HelloSQL2.0/refs/heads/main/01_cancellation_rates/data.tsv";
+download(url, "users.tsv")
 
+begin
+    using Pkg
+    Pkg.add(["CSV", "DataFrames"])
+    using CSV
+    using DataFrames
+end
+
+users = CSV.read("users.tsv", DataFrame; delim = '\t')
+println("$users")
