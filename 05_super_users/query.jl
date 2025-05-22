bed9a23d532a3af0a8fb6035e725d5029a4d97bd
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-22 16:41:06 -0600

    Update query.jl

--- 05_super_users/query.jl
+++ 05_super_users/query.jl
@@ -1,1 +1,12 @@
+begin
+    using Pkg
+    Pkg.add(["CSV", "DataFrames", "Dates", "ShiftedArrays"])
+    using CSV
+    using DataFrames
+    using Dates
+    using ShiftedArrays
+end
 
+url = "https://raw.githubusercontent.com/Uriel1201/HelloDATA/refs/heads/main/04_time_difference/data.tsv"
+download(url, "users.tsv")
+users = CSV.read("users.tsv", DataFrame; delim = '\t')
