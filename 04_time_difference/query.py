1dad5f1c3290705acdad008bd6902321f6aa58e5
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-22 01:51:45 -0600

    Update query.py

--- 04_time_difference/query.py
+++ 04_time_difference/query.py
@@ -1,30 +1,32 @@
 # python -m pip install oracledb numpy pyarrow polars --upgrade
-import numpy as np
-import polars as pl
-import oracledb
-import pyarrow
+# import numpy as np
+# import polars as pl
+# import oracledb
+# import pyarrow
 
 try:
-    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table="SELECT * FROM USERS_P4"
-    odf=conn.fetch_df_all(statement=table,arraysize=100)
-    pyarrow_table=pyarrow.Table.from_arrays(odf.column_arrays(),names=odf.column_names())
-    users=pl.from_arrow(pyarrow_table)
-    durations=(polars_users.sort(by=['ID','ACTION_DATE']
-                                     ,descending=[False,True]
-                            )
-                           .with_columns(ELAPSED_TIME=pl.col('ACTION_DATE')
-                                                        .diff(-1)
-                                                        .dt
-                                                        .total_days()
-                                                        .over(partition_by='ID')
-                            )
-                           .group_by('ID')
-                           .first()
-                           .select(pl.col('ID'),
-                                   pl.col('ELAPSED_TIME')
-                            )
-    )
-    print(f'Returning time elapsed using Polars:\n{durations}')          
+    # You need to write your credentials in the following connection.
+    conn = oracledb.connect(user = "[Username]", password = "[Password]", dsn = "localhost:1521/FREEPDB1")
+    table = "SELECT * FROM USERS_P4"
+    odf = conn.fetch_df_all(statement = table, arraysize = 100)
+    pyarrow_table = pyarrow.Table.from_arrays(odf.column_arrays(), names = odf.column_names())
+    users = pl.from_arrow(pyarrow_table).lazy()
+    
+    durations = (users.sort(by = ['ID','ACTION_DATE']
+                            ,descending=[False,True]
+                       )
+                      .with_columns(ELAPSED_TIME = pl.col('ACTION_DATE')
+                                                     .diff(-1)
+                                                     .dt
+                                                     .total_days()
+                                                     .over(partition_by = 'ID')
+                       )
+                      .group_by('ID')
+                      .first()
+                      .select(pl.col('ID'),
+                              pl.col('ELAPSED_TIME')
+                       )
+                )
+    print(f'Returning time elapsed:\n{durations}')          
 finally:
     conn.close()
