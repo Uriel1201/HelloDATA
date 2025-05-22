3e83628f18e001d34a0888c212a963d2c5b511e7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-22 16:04:05 -0600

    Update query.py

--- 05_super_users/query.py
+++ 05_super_users/query.py
@@ -11,6 +11,17 @@ try:
     odf = conn.fetch_df_all(statement = table, arraysize=100)
     pyarrow_table = pyarrow.Table.from_arrays(odf.column_arrays(), names = odf.column_names())
     users = pl.from_arrow(pyarrow_table)
+
+    '''
+    Alternative 2: Querying directly from this repository 
+    url = "https://raw.githubusercontent.com/Uriel1201/HelloDATA/refs/heads/main/05_super_users/data.tsv"
+    users = pl.scan_csv(url,
+                        separator = "\t",
+                        has_header = True,
+                        infer_schema_length = 1000,
+                        ignore_errors = False
+               )
+    '''
     
     superusers = (users.sort(by=['USER_ID','TRANSACTION_DATE'])
                        .with_columns(SUPERUSER_DATE = pl.col('TRANSACTION_DATE')
