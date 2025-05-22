a781f619bdcf7626a79216a4e51bb70667128ce8
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-21 18:35:40 -0600

    Update query.py

--- 03_frequent_item/query.py
+++ 03_frequent_item/query.py
@@ -10,7 +10,28 @@ try:
     table = "SELECT * FROM ITEMS_P3"
     odf = conn.fetch_df_all(statement = table, arraysize = 100)
     pyarrow_table = pyarrow.Table.from_arrays(odf.column_arrays(), names = odf.column_names())
-    items = pl.from_arrow(pyarrow_table)
+    items = pl.from_arrow(pyarrow_table).lazy()
+
+    '''
+    Alternative 2: Querying directly from this repository 
+    url = "https://raw.githubusercontent.com/Uriel1201/HelloDATA/refs/heads/main/03_frequent_item/data.tsv"
+    items = pl.scan_csv(url,
+                        separator = "\t",
+                        has_header = True,
+                        infer_schema_length = 1000,
+                        ignore_errors = False
+               )
+    '''
+
+    sample = items.head(5)
+    print(f'Items table SAMPLE(5):\n{sample.collect()}')
+
+    df = (sample.group_by(['DATES','ITEM'])
+                .agg(pl.len()
+                       .alias('COUNT')
+                 )
+         )
+    print(f'\nNumber of itemes by each date SAMPLE:\n{df}')
     
     counts = (items.group_by(['DATES','ITEM'])
                    .agg(pl.len()
@@ -26,6 +47,6 @@ try:
                            pl.col('ITEM')
                     )
              )
-    print(f'item most frequented by each date:\n{counts}')          
+    print(f'\nMost frequented item by each date:\n{counts.collect()}')          
 finally:
     conn.close()
