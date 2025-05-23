ef5c2cf98e29df0bb78ce4df63bc8759ead60ae8
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-22 21:55:39 -0600

    Update query.py

--- 06_content_recomm/query.py
+++ 06_content_recomm/query.py
@@ -5,38 +5,39 @@ import oracledb
 import pyarrow
 
 try:
-    conn=oracledb.connect(user="[Username]", password="[Password]", dsn="localhost:1521/FREEPDB1")
-    table1="SELECT * FROM FRIENDS_P6"
-    table2="SELECT * FROM LIKES_P6"
-    odf1=conn.fetch_df_all(statement=table1,arraysize=100)
-    odf2=conn.fetch_df_all(statement=table2,arraysize=100)
-    pyarrow_table1=pyarrow.Table.from_arrays(odf1.column_arrays(),names=odf1.column_names())
-    pyarrow_table2=pyarrow.Table.from_arrays(odf2.column_arrays(),names=odf2.column_names())
-    friends=pl.from_arrow(pyarrow_table1).lazy()
-    likes=pl.from_arrow(pyarrow_table2).lazy()
-    lf=(friends.join(likes.select(pl.col('USER_ID')
-                                    .alias('FRIEND'),
-                                  pl.col('PAGE_LIKES')
-                           )
-                     ,on='FRIEND'
-                     ,how='right'
-                )
-               .select(pl.col('USER_ID'),
-                       pl.col('PAGE_LIKES')
-                )
-    )
-    recommendations=(lf.join(likes,
-                             on=['USER_ID','PAGE_LIKES'],
-                             how='anti'
-                        )
-                       .unique()
-                       .select(pl.col('USER_ID'),
-                               pl.col('PAGE_LIKES')
-                                 .alias('RECOMMENDATION')
-                        )
-                       .sort(by='USER_ID')
-
-    ).collect()
-    print(f'Recommendations:\n{recommendations}')          
+    # You need to write your credentials in the following connection.
+    conn = oracledb.connect(user = "[Username]", password = "[Password]", dsn = "localhost:1521/FREEPDB1")
+    table1 = "SELECT * FROM FRIENDS_P6"
+    table2 = "SELECT * FROM LIKES_P6"
+    odf1 = conn.fetch_df_all(statement = table1, arraysize = 100)
+    odf2 = conn.fetch_df_all(statement = table2, arraysize = 100)
+    pyarrow_table1 = pyarrow.Table.from_arrays(odf1.column_arrays(), names = odf1.column_names())
+    pyarrow_table2 = pyarrow.Table.from_arrays(odf2.column_arrays(), names = odf2.column_names())
+    friends = pl.from_arrow(pyarrow_table1).lazy()
+    likes = pl.from_arrow(pyarrow_table2).lazy()
+    
+    lf = (friends.join(likes.select(pl.col('USER_ID')
+                                      .alias('FRIEND'),
+                                    pl.col('PAGE_LIKES')
+                             )
+                       ,on = 'FRIEND'
+                       ,how = 'right'
+                  )
+                 .select(pl.col('USER_ID'),
+                         pl.col('PAGE_LIKES')
+                  )
+         )
+    recommendations = (lf.join(likes,
+                               on = ['USER_ID','PAGE_LIKES'],
+                               how = 'anti'
+                          )
+                         .unique()
+                         .select(pl.col('USER_ID'),
+                                 pl.col('PAGE_LIKES')
+                                   .alias('RECOMMENDATION')
+                          )
+                         .sort(by = 'USER_ID')
+                      )
+    print(f'\nRecommendations:\n{recommendations.collect()}')          
 finally:
     conn.close()
