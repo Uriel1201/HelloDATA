282ed5f35e5dde670acd85d7012b0e350a106c84
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-22 21:41:40 -0600

    Update query.sql

--- 06_content_recomm/query.sql
+++ 06_content_recomm/query.sql
@@ -1,10 +1,12 @@
 /*  
 06. Content Recommendations
 
-Writing a query to return page recommendations
+Using the following two tables, write a
+query to return page recommendations
 to a social media user based on the
 pages that their friends have liked, but
-that they have not yet marked as liked. */
+that they have not yet marked as liked.
+Order the result by ascending user ID. */
 
 /* Querying the original data*/
 SELECT
