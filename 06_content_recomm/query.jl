58470bc0b710c202092608cae46a6877055f2f6a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-23 00:14:49 -0600

    Update query.jl

--- 06_content_recomm/query.jl
+++ 06_content_recomm/query.jl
@@ -11,3 +11,36 @@ download(url_1, "friends.tsv")
 download(url_2, "likes.tsv")
 friends = CSV.read("friends.tsv", DataFrame; delim = '\t')
 likes = CSV.read("likes.tsv", DataFrame; delim = '\t')
+
+s_friends = first(friends, 5)
+s_likes = first(likes, 5)
+println("\nFRIENDS TABLE (SAMPLE):\n$s_friends")
+println("\nLIKES TABLE (SAMPLE):\n$s_likes")
+
+s_jl = rename!(innerjoin(s_friends, 
+                         rename(s_likes, 
+                                :USER_ID => :FRIEND
+                               ), 
+                         on = :FRIEND
+                        ),
+               :PAGE_LIKES => :POSSIBLE
+              )
+println("\n IDENTIFYING POTENTIAL RECOMMENDATIONS (SAMPLE):\n$s_jl")
+
+jl = select(innerjoin(friends, 
+                      rename(likes, 
+                             :USER_ID => :FRIEND
+                            ), 
+                      on = :FRIEND
+                     ),
+            :USER_ID,
+            :PAGE_LIKES
+           )
+recommendations = antijoin(jl, 
+                           likes, 
+                           on = [:USER_ID, :PAGE_LIKES]
+                          )
+rename!(unique!(recommendations),
+        :PAGE_LIKES => :RECOMMENDATIONS
+       )
+println("\n RECOMMENDATIONS:\n$recommendations")
