a7434fe8bc83bc2c42629e1ef96331ff6cd6acea
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-27 14:56:15 -0600

    Update query.jl

--- 03_frequent_item/query.jl
+++ 03_frequent_item/query.jl
@@ -1,40 +1,95 @@
 begin
     using Pkg
-    Pkg.add(["CSV", "DataFrames"])
-    using CSV
+    Pkg.add(["DataFrames", "SQLite", "Dates"])
     using DataFrames
+    using SQLite
+    using Dates
 end
 
-url = "https://raw.githubusercontent.com/Uriel1201/HelloDATA/refs/heads/main/03_frequent_item/data.tsv";
-download(url, "items.tsv")
-items = CSV.read("items.tsv", DataFrame; delim = '\t')
+db = SQLite.DB()
 
-sample = first(items, 5)
-println(" Items table (SAMPLE = 5):\n$sample")
+#*/________________________________
 
-df = combine(groupby(sample,
-                     [:DATES, :ITEM]
-             ),
-             :ITEM => length => :COUNT
-     )
-println("\nNumber of items by date (SAMPLE):\n$df")
+schema = Tables.Schema((:ITEM, :DATES), (String, String))
+SQLite.createtable!(db, "ITEMS", schema, temp = false)
+rows = [("apple", "01-jan-20"),
+        ("apple", "01-jan-20"),
+        ("pear", "01-jan-20"),
+        ("pear", "01-jan-20"),
+        ("pear", "02-jan-20"),
+        ("pear", "02-jan-20"),
+        ("pear", "02-jan-20"),
+        ("orange", "02-jan-20")]
 
-max_items = combine(groupby(items,
-                            [:DATES, :ITEM]
-                           ),
-                    :ITEM => length => :COUNT
-                   )
-transform!(groupby(max_items,
-                   :DATES
-                  ),
-           :COUNT => maximum => :MAX_COUNT
-          )
-result = sort(select(filter(row -> row.COUNT == row.MAX_COUNT,
-                            max_items
-                           ),
-                     :DATES,
-                     :ITEM
-                    ),
-              :DATES
-             )
-println("\nMost frequented item by each date:\n$result")
+SQLite.execute(db, "BEGIN TRANSACTION")
+placeholders = join(["(?, ?)" for _ in rows], ", ")
+query = "INSERT INTO ITEMS (ITEM, DATES) VALUES $placeholders"
+stmt = SQLite.Stmt(db, query)
+params = collect(Iterators.flatten(rows))
+DBInterface.execute(stmt, params)
+SQLite.execute(db, "COMMIT")
+
+#*/________________________________
+
+const DATE_REGEX = r"-(\d{2})"i
+const SPACE = "********************************"
+
+#*/________________________________
+
+function format_date(match)
+    year = "20" * match[2:3]
+    return "-" * year
+end
+
+#*/________________________________
+
+function main(args)
+
+    database = uppercase(args)
+    println("$SPACE\nWORKING WITH A $database DATABASE\n$SPACE")
+
+    items = DBInterface.execute(db, "SELECT * FROM ITEMS") |> DataFrame
+    items.DATES .= replace.(items.DATES, DATE_REGEX => format_date)
+    items[!, :DATES] = Date.(items.DATES, "dd-u-yyyy")
+
+    sample = first(items, 5)
+    println("\n$SPACE")
+    println("ITEMS TABLE -> SAMPLE:\n$sample")
+
+    df = combine(groupby(sample,
+                         [:DATES, :ITEM]
+                 ),
+                 :ITEM => length => :COUNT
+         )
+    println("\n$SPACE")
+    println("Number of items by each date -> SAMPLE:\n$df")
+
+    max_items = combine(groupby(items,
+                                [:DATES, :ITEM]
+                               ),
+                        :ITEM => length => :COUNT
+                       )
+    transform!(groupby(max_items,
+                       :DATES
+                      ),
+               :COUNT => maximum => :MAX_COUNT
+              )
+    result = sort(select(filter(row -> row.COUNT == row.MAX_COUNT,
+                                max_items
+                               ),
+                         :DATES,
+                         :ITEM
+                        ),
+                  :DATES
+                 )
+    println("\n$SPACE")
+    println("Most frequented item by each date:\n$result")
+end
+
+#*/________________________________
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main(ARGS)
+else
+    println("\n ARCHIVE LOADED AS MODULE, EXECUTE MAIN() MANUALLY WITH SQLITE AS ARG.")
+end
