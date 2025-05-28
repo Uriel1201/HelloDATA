ff19f0e28114778d17f5843b41490270a8b1bcd2
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-05-28 12:18:16 -0600

    Update query.jl

--- 03_frequent_item/query.jl
+++ 03_frequent_item/query.jl
@@ -5,80 +5,126 @@ begin
     using SQLite
     using Dates
 end
+#=
+**********************************************
+=#
+struct DatabaseConfig
 
-db = SQLite.DB()
+    db_path::String
 
-schema = Tables.Schema((:ITEM, :DATES), (String, String))
-SQLite.createtable!(db, "ITEMS", schema, temp = false)
-rows = [("apple", "01-jan-20"),
-        ("apple", "01-jan-20"),
-        ("pear", "01-jan-20"),
-        ("pear", "01-jan-20"),
-        ("pear", "02-jan-20"),
-        ("pear", "02-jan-20"),
-        ("pear", "02-jan-20"),
-        ("orange", "02-jan-20")]
+end
+#=
+**********************************************
+=#
+function database_connection(f::Function, config::DatabaseConfig)
 
-SQLite.execute(db, "BEGIN TRANSACTION")
-placeholders = join(["(?, ?)" for _ in rows], ", ")
-query = "INSERT INTO ITEMS (ITEM, DATES) VALUES $placeholders"
-stmt = SQLite.Stmt(db, query)
-params = collect(Iterators.flatten(rows))
-DBInterface.execute(stmt, params)
-SQLite.execute(db, "COMMIT")
+    db = SQLite.DB(config.db_path)
+    try
+        @info "Connection is established"
+        f(db)
+    catch e
+        @error "Error in operation" exception=(e, catch_backtrace())
+        rethrow()
+    finally
+        SQLite.close(db)
+        @info "Connection is closed"
+    end
 
-const DATE_REGEX = r"-(\d{2})"i
-const SPACE = "********************************"
+end
+#=
+**********************************************
+=#
+function get_items(db::SQLite.DB)
 
+    schema = Tables.Schema((:ITEM, :DATES), (String, String))
+    SQLite.createtable!(db, "ITEMS", schema, temp = false)
+    rows = [("apple", "01-jan-20"),
+            ("apple", "01-jan-20"),
+            ("pear", "01-jan-20"),
+            ("pear", "01-jan-20"),
+            ("pear", "02-jan-20"),
+            ("pear", "02-jan-20"),
+            ("pear", "02-jan-20"),
+            ("orange", "02-jan-20")]
+    SQLite.execute(db, "BEGIN TRANSACTION")
+    placeholders = join(["(?, ?)" for _ in rows], ", ")
+    query = "INSERT INTO ITEMS (ITEM, DATES) VALUES $placeholders"
+    stmt = SQLite.Stmt(db, query)
+    params = collect(Iterators.flatten(rows))
+    DBInterface.execute(stmt, params)
+    SQLite.execute(db, "COMMIT")
+    @info "items is available"
+
+end
+#=
+**********************************************
+=#
 function format_date(match)
     year = "20" * match[2:3]
     return "-" * year
 end
+#=
+**********************************************
+=#
+const DATE_REGEX = r"-(\d{2})"i
+const SPACE = "********************************"
+#=
+**********************************************
+=#
+function main(args)
 
-function main(args = String())
-    database = uppercase(args[1])
-    println("$SPACE\nWORKING WITH $database DATABASE\n$SPACE")
-    table = uppercase(args[2])
-    items = DBInterface.execute(db, "SELECT * FROM $table") |> DataFrame
-    items.DATES .= replace.(items.DATES, DATE_REGEX => format_date)
-    items[!, :DATES] = Date.(items.DATES, "dd-u-yyyy")
+    database = uppercase(args)
+    println("$SPACE\nWORKING WITH $database DATABASE\n$SPACE\n")
+    config = DatabaseConfig(":memory:")
 
-    sample = first(items, 5)
-    println("\n$SPACE")
-    println("ITEMS TABLE -> SAMPLE:\n$sample")
+    database_connection(config) do db
 
-    df = combine(groupby(sample,
-                         [:DATES, :ITEM]
-                 ),
-                 :ITEM => length => :COUNT
-         )
-    println("\n$SPACE")
-    println("Number of items by each date -> SAMPLE:\n$df")
+        get_items(db)
+        items = DBInterface.execute(db, "SELECT * FROM ITEMS") |> DataFrame
+        items.DATES .= replace.(items.DATES, DATE_REGEX => format_date)
+        items[!, :DATES] = Date.(items.DATES, "dd-u-yyyy")
 
-    max_items = combine(groupby(items,
-                                [:DATES, :ITEM]
-                               ),
-                        :ITEM => length => :COUNT
-                       )
-    transform!(groupby(max_items,
-                       :DATES
-                      ),
-               :COUNT => maximum => :MAX_COUNT
-              )
-    result = sort(select(filter(row -> row.COUNT == row.MAX_COUNT,
-                                max_items
-                               ),
-                         :DATES,
-                         :ITEM
-                        ),
-                  :DATES
-                 )
-    println("\n$SPACE")
-    println("Most frequented item by each date:\n$result")
+        sample = first(items, 5)
+        println("\n$SPACE")
+        println("ITEMS TABLE -> SAMPLE:\n$sample")
+
+        df = combine(groupby(sample,
+                             [:DATES, :ITEM]
+                            ),
+                     :ITEM => length => :COUNT
+                    )
+        println("\n$SPACE")
+        println("Number of items by each date -> SAMPLE:\n$df")
+
+        max_items = combine(groupby(items,
+                                    [:DATES, :ITEM]
+                                   ),
+                            :ITEM => length => :COUNT
+                           )
+        transform!(groupby(max_items,
+                           :DATES
+                          ),
+                   :COUNT => maximum => :MAX_COUNT
+                  )
+        result = sort(select(filter(row -> row.COUNT == row.MAX_COUNT,
+                                    max_items
+                                   ),
+                             :DATES,
+                             :ITEM
+                            ),
+                      :DATES
+                     )
+        println("\n$SPACE")
+        println("Most frequented item by each date:\n$result\n")
+
+    end
+
 end
-
+#=
+**********************************************
+=#
 if abspath(PROGRAM_FILE) == @__FILE__
     main(ARGS)
 else
-    println("\n ARCHIVE LOADED AS MODULE, EXECUTE MAIN() MANUALLY WITH SQLITE AND ITEMS AS ARGS.")
+    println("\n ARCHIVE LOADED AS MODULE, EXECUTE MAIN() MANUALLY WITH 'SQLITE'AS ARG.")
 end
