f8f9f19bbce0ccd15296a9935d26d0e3de874ffd
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-04 13:42:17 -0600

    Update query.sql

--- 01_cancellation_rates/query.sql
+++ 01_cancellation_rates/query.sql
@@ -6,13 +6,17 @@ actions, and dates, write a query to
 return the publication and cancellation
 rate for each user. */
 
-/*Querying original data. */
+/* ORACLE. */
+
+/********************************************************************/
+
+-- Querying original data.
 SELECT
     *
 FROM
     USERS_P1;
 
-        /*Returning rates for each user. */
+        -- Returning rates for each user.
 WITH TOTALS ( -- Totals for each action 
     USER_ID,
     TOTAL_STARTS,
@@ -58,3 +62,26 @@ SELECT
           2) AS CANCEL_RATE
 FROM
     TOTALS;
+
+/* DUCKDB. */
+
+/********************************************************************/
+
+SELECT * FROM USERS;
+
+WITH TOTALS AS (
+    SELECT
+        USER_ID,
+        SUM(IF(ACTION = 'start',1,0)) AS TOTAL_STARTS,
+        SUM(IF(ACTION = 'cancel',1,0)) AS TOTAL_CANCELS,
+        SUM(IF(ACTION = 'publish',1,0)) AS TOTAL_PUBLISHES
+    FROM
+        USERS
+    GROUP BY
+        USER_ID) SELECT
+                     USER_ID,
+                     ROUND(TOTAL_PUBLISHES / NULLIF(TOTAL_STARTS, 0), 2) AS PUBLISH_RATE,
+                     ROUND(TOTAL_CANCELS / NULLIF(TOTAL_STARTS, 0), 2) AS CANCEL_RATE
+                 FROM
+                     TOTALS
+                 ORDER BY 1;
