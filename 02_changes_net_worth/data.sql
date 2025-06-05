7eb15657bc94a9debe4b991f28c3f984e7794c86
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-04 22:14:52 -0600

    Update data.sql

--- 02_changes_net_worth/data.sql
+++ 02_changes_net_worth/data.sql
@@ -6,6 +6,9 @@ between two users, write a query to
 return the change in net worth for each
 user, ordered by decreasing net change. */
 
+/* ORACLE. */
+
+/********************************************************************/
 CREATE TABLE TRANSACTIONS_P2 (
     SENDER           INTEGER,
     RECEIVER         INTEGER,
@@ -75,3 +78,20 @@ INSERT INTO TRANSACTIONS_P2
         *
     FROM
         NAMES;
+
+/* DUCKDB. */
+
+/********************************************************************/
+CREATE TABLE TRANSACTIONS (SENDER           INTEGER,
+                           RECEIVER         INTEGER,
+                           AMOUNT           DECIMAL,
+                           TRANSACTION_DATE DATE
+                          );
+
+INSERT INTO TRANSACTIONS  BY POSITION VALUES (5, 2, 10.0, "2020-02-12"),
+                                             (1, 3, 15.0, "2020-02-13"),
+                                             (2, 1, 20.0, "2020-02-13"),
+                                             (2, 3, 25.0, "2020-02-14"),
+                                             (3, 1, 20.0, "2020-02-15"),
+                                             (3, 2, 15.0, "2020-02-15"),
+                                             (1, 4, 5.0, "2020-02-16");
