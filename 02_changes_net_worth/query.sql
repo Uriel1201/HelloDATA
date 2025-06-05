1515de752856fb3b8fbfe60e7f65b2705bae580a
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-04 22:24:08 -0600

    Update query.sql

--- 02_changes_net_worth/query.sql
+++ 02_changes_net_worth/query.sql
@@ -6,13 +6,16 @@ between two users, write a query to
 return the change in net worth for each
 user, ordered by decreasing net change. */
 
-/* Querying original data*/
+/* ORACLE. */
+
+/********************************************************************/
+-- Querying original data
 SELECT
     *
 FROM
     TRANSACTIONS_P2;
 
-        /* Querying the net change of each user*/
+        -- Querying the net change of each user
 WITH SENDERS ( -- Table of Senders and their sended amounts
     SENDER,
     SENDED
@@ -44,3 +47,29 @@ FROM
     FULL OUTER JOIN SENDERS   S ON R.RECEIVER = S.SENDER
 ORDER BY
     2 DESC;
+
+/* DUCKDB. */
+
+/********************************************************************/
+SELECT * FROM TRANSACTIONS;
+
+WITH SENDERS AS (SELECT
+                     SENDER,
+                     SUM(AMOUNT) AS SENDED
+                 FROM 
+                     TRANSACTIONS
+                 GROUP BY
+                     SENDER),
+RECEIVERS AS (SELECT
+                  RECEIVER,
+                  SUM(AMOUNT) AS RECEIVED
+              FROM
+                  TRANSACTIONS
+              GROUP BY
+                  RECEIVER) SELECT
+                                COALESCE(S.SENDER, R.RECEIVER) AS USER_ID,
+                                COALESCE(R.RECEIVED, 0) - COALESCE(S.SENDED, 0) AS NET_CHANGE
+                            FROM
+                                RECEIVERS R
+                            FULL JOIN SENDERS S ON (R.RECEIVER = S.SENDER)
+                            ORDER BY 2 DESC;
