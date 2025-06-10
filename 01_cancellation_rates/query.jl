2964c03cabed0b872e95af246cddb7bd36e05155
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-10 13:19:57 -0600

    Update query.jl

--- 01_cancellation_rates/query.jl
+++ 01_cancellation_rates/query.jl
@@ -1,208 +1,59 @@
+#=
 using Pkg
-Pkg.add(["DataFrames", "Arrow", "SQLite", "Downloads", "CSV", "DuckDB"])
 
+packages = ["SQLite", "Tables", "DataFrames", "Arrow", "Downloads", "DuckDB"]
 
-module SQLiteArrowKit
+for pkg in packages
 
-using DataFrames, Arrow, SQLite, Downloads, CSV, DuckDB
-
-export sqlite_connection, is_available, get_ArrowTable, get_DataFrame
-
-const DB_PATH = "heart.db"
-#=
-**********************************************
-=#
-function is_available(db::SQLite.DB, table::String)::Bool
-
-    name = uppercase(table)
-    list_tables = collect(SQLite.tables(db))
-    names = [t.name for t in list_tables]
-
-    return name in names
+    Pkg.add(pkg)
 
 end
-#=
-**********************************************
 =#
-function get_ArrowTable(db::SQLite.DB, table::String)::Arrow.Table
 
-    name = uppercase(table)
-    io = IOBuffer()
+DB_PATH = "my_SQLite.db"
 
-    query = DBInterface.execute(db, "SELECT * FROM $name")
-    start_write = time()
-    Arrow.write(io, query)
-    end_write = time()
-    start_arrow = time()
-    seekstart(io)
-    arrow_table = Arrow.Table(io)
-    end_arrow = time()
-    time_writing = round(end_write - start_write, digits = 4)
-    time_reading = round(end_arrow - start_arrow, digits = 4)
-    @info "arrow table created:" time_writing time_reading
-    return arrow_table
+using Downloads
 
-end
-#=
-**********************************************
-=#
-function get_DataFrame(db::SQLite.DB, table::String)::DataFrame
+arrowkit = "https://github.com/Uriel1201/HelloDATA/raw/refs/heads/main/SQLiteArrowKit.jl"
+Downloads.download(arrowkit,"arrowkit.jl")
+db_url = "https://github.com/Uriel1201/HelloDATA/raw/refs/heads/main/my_SQLite.jl"
+Downloads.download(db_url, "database.jl")
 
-    name = uppercase(table)
+include("database.jl")
+include("arrowkit.jl")
 
-    return DBInterface.execute(db, "SELECT * FROM $name") |> DataFrame
+using .MyDataBase, DataFrames, Arrow, SQLite, DuckDB, .SQLiteArrowKit
 
-end
+MyDataBase.main()
 #=
 **********************************************
 =#
 function main(args = ARGS)
 
     db = SQLite.DB(DB_PATH)
-    csv_path = "heart_2022_no_nans.csv"
-
-    csvstart = time()
-    heart_csv = CSV.read(csv_path, DataFrame)
-    csvend = time()
-    lecture_csv = round(csvend - csvstart, digits = 4)
-    println("\n", "*"^40)
-    println("LECTURE TIME, 'CSV TO DATAFRAME': $lecture_csv")
-    
-        if is_available(db, "heart_2022")
-
-            duck = nothing
-
-            try
-
-                duck = DBInterface.connect(DuckDB.DB, ":memory:")
-
-                println("\n", "*"^40)
-                start_cursor = time()
-                cursor_df = get_DataFrame(db, "heart_2022")
-                end_cursor = time()
-                lecture_cursor = end_cursor - start_cursor
-                println("LECTURE TIME, 'DBInterface.Cursor() TO DATAFRAMES': $lecture_cursor")
-            
-                arrowstart = time()
-                duck_heart = get_ArrowTable(db, "heart_2022")
-                arrowend = time()
-                DuckDB.register_data_frame(duck, duck_heart, "HEART")
-                state = """
-                        SELECT
-                            State,
-                            COUNT(*) AS PERSONS
-                         FROM
-                             'HEART'
-                         GROUP BY
-                             State
-                         LIMIT 10
-                         """
-                sex = """
-                      SELECT
-                          Sex,
-                          COUNT(*) AS PERSONS
-                      FROM
-                          'HEART'
-                      GROUP BY
-                          Sex
-                      """
-                sex_state = """
-                            SELECT
-                                State,
-                                Sex,
-                                COUNT(*) AS PERSONS
-                            FROM
-                                'HEART'
-                            GROUP BY
-                                State, Sex
-                            LIMIT 10
-                            """
-
-                state_duck = DBInterface.execute(duck, state) |> DataFrame
-                sex_duck = DBInterface.execute(duck, sex) |> DataFrame
-                statesex_duck = DBInterface.execute(duck, sex_state) |> DataFrame
-                arrow_end = time()
-                lecture_arrow = round(arrowend - arrowstart, digits = 4)
-                exec_arrow = round(arrow_end - arrowend, digits = 4)
-                println("\n", "*"^40)
-                println("TIME READING AND WRITING DBInterface.Cursor() => DOC.ARROW => ArrowTable: $lecture_arrow\nPROCESSING TIME (DUCKDB QUERIES): $exec_arrow")
-                println("\n$sex_duck\n")
-                println("\n$state_duck\n")
-                println("\n$statesex_duck\n")
-
-                df_start = time()
-                heart_state = combine(groupby(heart_csv, :State), nrow => :Persons)
-                heart_sex = combine(groupby(heart_csv, :Sex), nrow => :Persons)
-                sex_state = combine(groupby(heart_csv, [:State, :Sex]), nrow => :Persons)
-                sample_1 = first(heart_state, 10)
-                sample_2 = first(sex_state, 10)
-                df_end = time()
-                exec_df = df_end - df_start
-                println("\n", "*"^40)
-                println("PROCESSING TIME (DATAFRAMES): $exec_df")
-                println("\n$heart_sex\n")
-                println("\n$sample_1\n")
-                println("\n$sample_2\n")
-
-            finally
-
-                DBInterface.close!(duck)
-
-            end
-
-        end
-
-end
-#=
-**********************************************
-=#
-end
-#=
-**********************************************
-=#
-if Base.@isdefined(PROGRAM_FILE) &&
-   abspath(PROGRAM_FILE) == abspath(@__FILE__)
-
-    main()
-
-end
-
-
-module CancellationRates
-
-url = "https://github.com/Uriel1201/HelloDATA/raw/refs/heads/main/my_SQLite.db"
-db_path = "my_SQLite.db"
-Downloads.download(url, db_path)
-#=
-**********************************************
-=#
-function main(args = ARGS)
-
-    db = SQLite.DB(db_path)
 
     if is_available(db, args)
 
-        df_users = get_DataFrame(db, args)
+        df_users = SQLiteArrowKit.get_DataFrame(db, args)
         duck = nothing
 
         try
 
             duck = DBInterface.connect(DuckDB.DB, ":memory:")
-            arrow_users = get_ArrowTable(db, args)
 
-            DuckDB.register_data_frame(duck, arrow_users, "duck_users")
-            duck_users = DBInterface.execute(duck, "SELECT * FROM 'duck_users' USING SAMPLE 50% (bernoulli)") |> DataFrame
+            arrow_users = SQLiteArrowKit.get_ArrowTable(db, args)
+            DuckDB.register_data_frame(duck, arrow_users, "USERS")
+            duck_users = DBInterface.execute(duck, "SELECT * FROM USERS USING SAMPLE 50% (bernoulli)") |> DataFrame
             println("\n", "*"^40)
-            println("\nARROW_USERS USING DUCKDB QUERIES -> SAMPLE:\n$duck_users")
+            println("\nUSERS TABLE USING DUCKDB QUERIES -> SAMPLE:\n$duck_users")
 
-            start_duck = time()
             query = """
             WITH DUCK_UPDATED AS (
             SELECT
                 USER_ID,
                 ACTION,
                 STRFTIME(STRPTIME(DATES, '%d-%b-%y'), '%Y-%m-%d')::DATE AS DATES
-            FROM duck_users),
+            FROM USERS),
             TOTALS AS (
             SELECT
                 USER_ID,
@@ -224,26 +75,23 @@ function main(args = ARGS)
             TOTALS
             ORDER BY 1
             """
+
             duck_result = DBInterface.execute(duck, query) |> DataFrame
-            end_duck = time()
-            elapsed_duck = end_duck - start_duck
             println("\n", "*"^40)
-            println("\nUSER STATISTICS USING DUCKDB QUERIES:\n EXECUTION TIME: $elapsed_duck\n$duck_result\n")
+            println("\nUSER STATISTICS USING DUCKDB QUERIES:\n$duck_result")
 
             sample = first(df_users, 5)
             println("\n", "*"^40)
             println("\nUSERS DATAFRAME -> SAMPLE:\n$sample")
 
-            start_df = time()
             dummy = select(df_users, :USER_ID, [:ACTION => ByRow(isequal(v)) => Symbol(v) for v in unique(df_users.ACTION)])
             totals = combine(groupby(dummy, :USER_ID), names(dummy, Not(:USER_ID)) .=> sum)
             totals.CANCEL_RATE = @.ifelse(totals.start_sum != 0, totals.cancel_sum ./ totals.start_sum, 0.0)
             totals.PUBLISH_RATE = @.ifelse(totals.start_sum != 0, totals.publish_sum ./ totals.start_sum, 0.0)
             result = select(totals, :USER_ID, :CANCEL_RATE, :PUBLISH_RATE)
-            end_df = time()
-            elapsed_df = end_df - start_df
+            
             println("\n", "*"^40)
-            println("\nUSER STATISTICS USING DATAFRAMES TOOLS:\n EXECUTION TIME: $elapsed_df\n$result\n")
+            println("\nUSER STATISTICS USING DATAFRAMES TOOLS:\n EXECUTION TIME:\n$result")
 
         finally
 
@@ -253,12 +101,8 @@ function main(args = ARGS)
 
     else
 
-        println("$args TABLE NOT AVAILABLE IN $db_path")
+        println("$args TABLE NOT AVAILABLE IN $DB_PATH")
 
     end
 
-end
-#=
-**********************************************
-=#
 end
