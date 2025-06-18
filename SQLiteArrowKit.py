1cf0fdc2979baad121bc97e556607313cefc7326
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-18 14:52:19 -0600

    Update SQLiteArrowKit.py

--- SQLiteArrowKit.py
+++ SQLiteArrowKit.py
@@ -18,10 +18,11 @@ def is_available(conn:dbapi.AdbcSqliteConnection, table:str) -> bool:
 def get_ArrowTable(conn:dbapi.AdbcSqliteConnection, table:str) -> pa.Table:
 
     name = table.upper()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM " + name)
-     
-    return cursor.fetch_arrow_table()
+
+    with conn.cursor() as cursor:
+        
+        cursor.execute("SELECT * FROM " + name)
+        return cursor.fetch_arrow_table()
 #============================================================
 #THIS FUNCTION IS ONLY A TEST AND IT MUSTN'T BE EXECUTED
 def main():
