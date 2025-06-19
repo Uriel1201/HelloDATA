1f0fdbd94c3b5a62a8e4f9729a2f7badf699c6d7
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-18 19:40:52 -0600

    Update SQLiteArrowKit.py

--- SQLiteArrowKit.py
+++ SQLiteArrowKit.py
@@ -4,7 +4,7 @@ import polars as pol
 import pyarrow as pa
 import duckdb
 import time
-
+import pandas
 #============================================================
 def is_available(conn:dbapi.AdbcSqliteConnection, table:str) -> bool:
 
@@ -17,11 +17,23 @@ def is_available(conn:dbapi.AdbcSqliteConnection, table:str) -> bool:
 #============================================================
 def get_ArrowTable(conn:dbapi.AdbcSqliteConnection, table:str) -> pa.Table:
 
-    name = table.upper()
-    with conn.cursor() as cursor:
-        
-        cursor.execute("SELECT * FROM 'name'")
-        return cursor.fetch_arrow_table()
+    try:
+        with conn.cursor() as cursor:
+
+            if is_available(conn, table):
+
+                name = table.upper()
+                cursor.execute(f'SELECT * FROM {name}')
+                return cursor.fetch_arrow_table()
+
+            else:
+
+                cursor.execute(table)
+                return cursor.fetch_arrow_table()
+
+    except Exception as e:
+
+        print(f'CORRUPTED QUERY OR TABLE NOT AVAILABLE IN DATABASE: {e}')
 #============================================================
 #THIS FUNCTION IS ONLY A TEST AND IT MUSTN'T BE EXECUTED
 def main():
@@ -49,7 +61,7 @@ def main():
                                   .agg(pol.len().alias("persistenceInPopularity>95"))
                                   .filter(pol.col("persistenceInPopularity>95") > 5000)
     ).head(10)
-   
+
     end_polars = time.time()
     elap_polars = round(end_polars - start_polars, 4)
     print(f'\n{polMode}')
@@ -57,59 +69,68 @@ def main():
     print(f'\ntime~{elap_polars}')
 
     conn = dbapi.connect("file:/content/drive/MyDrive/popular.db?mode=ro")
-    if is_available(conn, "songs"):
 
-        try:
+    try:
 
-            duck = duckdb.connect(":memory:")
-            print("\n" + ":" * 50)
-            start_arrow = time.time()
-            arrow_songs = get_ArrowTable(conn, "songs")
-            end_arrow  = time.time()
-            elap_arr = round(end_arrow - start_arrow, 4)
-            print(f'LECTURE TIME, FROM CURSOR (NATIVE ARROW) TO ARROW TABLE: {elap_arr}')
+        duck = duckdb.connect(":memory:")
+        print("\n" + ":" * 50)
+        start_arrow = time.time()
+        arrow_songs = get_ArrowTable(conn, "songs")
+        query = """
+                SELECT
+                    name,
+                    mode
+                FROM
+                    SONGS
+                LIMIT 10
+        """
+        pandas_sample = get_ArrowTable(conn, query).to_pandas()
+        end_arrow  = time.time()
+        elap_arr = round(end_arrow - start_arrow, 4)
+        print(f'LECTURE TIME, FROM CURSOR (NATIVE ARROW) TO ARROW TABLE: {elap_arr}')
+        print(f'QUERYING DIRECTLY FROM SQLITE:\n{pandas_sample}')
+        print("\n" + ":" * 50)
+        print(f'PROCESSING TIME, USING DUCKDB QUERIES:')
+        duck_start = time.time()
+        mode =  """
+                SELECT
+                    mode,
+                    COUNT(*) AS frequency
+                FROM
+                    arrow_songs
+                GROUP BY
+                    mode
+        """
+        modePopularity = """
+                         WITH
+                             POPULAR AS(
+                                 SELECT
+                                     name,
+                                     mode,
+                                     COUNT(*) AS persistenceInPopularity95
+                                 FROM
+                                     arrow_songs
+                                 WHERE
+                                     popularity > 95
+                                 GROUP BY
+                                     spotify_id, name, mode)
+                         SELECT
+                             *
+                         FROM
+                             POPULAR
+                         WHERE
+                             persistenceInPopularity95 > 5000
+        """
+        duck.sql(mode).show()
+        duck.sql(modePopularity).show()
+        duck_end = time.time()
+        elap_duck = round(duck_end - duck_start, 4)
+        print(f'time~{elap_duck}')
 
-            print(f'PROCESSING TIME, USING DUCKDB QUERIES:')
-            duck_start = time.time()
-            mode =  """
-                    SELECT
-                        mode,
-                        COUNT(*) AS frequency
-                    FROM
-                        arrow_songs
-                    GROUP BY
-                        mode
-            """
-            modePopularity = """
-                             WITH
-                                 POPULAR AS(
-                                     SELECT
-                                         name,
-                                         mode,
-                                         COUNT(*) AS persistenceInPopularity95
-                                     FROM
-                                         arrow_songs
-                                     WHERE
-                                         popularity > 95
-                                     GROUP BY
-                                         spotify_id, name, mode)
-                             SELECT
-                                 *
-                             FROM
-                                 POPULAR
-                             WHERE
-                                 persistenceInPopularity95 > 5000
-            """
-            duck.sql(mode).show()
-            duck.sql(modePopularity).show()
-            duck_end = time.time()
-            elap_duck = round(duck_end - duck_start, 4)
-            print(f'time~{elap_duck}')
+    finally:
 
-        finally:
-
-            duck.close()
-            conn.close()
+        duck.close()
+        conn.close()
 #============================================================
 if __name__ == '__main__':
     main()
