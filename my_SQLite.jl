323cd638c6627d890903213625e7de7f866abf50
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-20 13:57:50 -0600

    Update my_SQLite.jl

--- my_SQLite.jl
+++ my_SQLite.jl
@@ -1,6 +1,6 @@
 module MyDataBase
 
-using SQLite, Tables
+using SQLite, Tables, Main.SQLiteArrowKit
 #=
 **********************************************
 =#
@@ -39,27 +39,35 @@ end
 =#
 function users_01(db::SQLite.DB)
 
-    schema = Tables.Schema((:USER_ID, :ACTION, :DATES), (Int32, String, String))
-    SQLite.createtable!(db, "USERS_01", schema, temp = false)
+    if !SQLiteArrowKit.is_available(db, "users_01")
 
-    rows = [(1, "start", "01-jan-20"),
-            (1, "cancel", "02-jan-20"),
-            (2, "start", "03-jan-20"),
-            (2, "publish", "04-jan-20"),
-            (3, "start", "05-jan-20"),
-            (3, "cancel", "06-jan-20" ),
-            (1, "start", "07-jan-20"),
-            (1, "publish", "08-jan-20")]
+        schema = Tables.Schema((:USER_ID, :ACTION, :DATES), (Int32, String, String))
+        SQLite.createtable!(db, "USERS_01", schema, temp = false)
 
-    placeholders = join(["(?, ?, ?)" for _ in rows], ", ")
-    query = "INSERT INTO USERS_01 (USER_ID, ACTION, DATES) VALUES $placeholders"
-    stmt = SQLite.Stmt(db, query)
-    params = collect(Iterators.flatten(rows))
-    DBInterface.execute(stmt, params)
+        rows = [(1, "start", "01-jan-20"),
+                (1, "cancel", "02-jan-20"),
+                (2, "start", "03-jan-20"),
+                (2, "publish", "04-jan-20"),
+                (3, "start", "05-jan-20"),
+                (3, "cancel", "06-jan-20" ),
+                (1, "start", "07-jan-20"),
+                (1, "publish", "08-jan-20")]
 
-    columns = join(schema.names, " | ")
-    _type = join(schema.types, " | ")
-    @info "TABLE USERS_01 IS AVAILABLE:" columns _type
+        placeholders = join(["(?, ?, ?)" for _ in rows], ", ")
+        query = "INSERT INTO USERS_01 (USER_ID, ACTION, DATES) VALUES $placeholders"
+        stmt = SQLite.Stmt(db, query)
+        params = collect(Iterators.flatten(rows))
+        DBInterface.execute(stmt, params)
+
+        columns = join(schema.names, " | ")
+        _type = join(schema.types, " | ")
+        @info "TABLE USERS_01 IS AVAILABLE:" columns _type
+
+    else
+
+        @info "TABLE USERS_01 ALREADY EXISTS"
+
+    end
 
 end
 #=
@@ -67,26 +75,33 @@ end
 =#
 function transactions_02(db::SQLite.DB)
 
-    schema = Tables.Schema((:SENDER, :RECEIVER, :AMOUNT, :TRANSACTION_DATE), (Int32, Int32, Float64, String))
-    SQLite.createtable!(db, "TRANSACTIONS_02" , schema, temp = false)
+    if !SQLiteArrowKit.is_available(db, "transactions_02")
+        schema = Tables.Schema((:SENDER, :RECEIVER, :AMOUNT, :TRANSACTION_DATE), (Int32, Int32, Float64, String))
+        SQLite.createtable!(db, "TRANSACTIONS_02" , schema, temp = false)
 
-    rows = [(5, 2, 10.0, "12-feb-20"),
-            (1, 3, 15.0, "13-feb-20"),
-            (2, 1, 20.0, "13-feb-20"),
-            (2, 3, 25.0, "14-feb-20"),
-            (3, 1, 20.0, "15-feb-20"),
-            (3, 2, 15.0, "15-feb-20"),
-            (1, 4, 5.0, "16-feb-20")]
+        rows = [(5, 2, 10.0, "12-feb-20"),
+                (1, 3, 15.0, "13-feb-20"),
+                (2, 1, 20.0, "13-feb-20"),
+                (2, 3, 25.0, "14-feb-20"),
+                (3, 1, 20.0, "15-feb-20"),
+                (3, 2, 15.0, "15-feb-20"),
+                (1, 4, 5.0, "16-feb-20")]
 
-    placeholders = join(["(?, ?, ?, ?)" for _ in rows], ", ")
-    query = "INSERT INTO TRANSACTIONS_02 (SENDER, RECEIVER, AMOUNT, TRANSACTION_DATE) VALUES $placeholders"
-    stmt = SQLite.Stmt(db, query)
-    params = collect(Iterators.flatten(rows))
-    DBInterface.execute(stmt, params)
+        placeholders = join(["(?, ?, ?, ?)" for _ in rows], ", ")
+        query = "INSERT INTO TRANSACTIONS_02 (SENDER, RECEIVER, AMOUNT, TRANSACTION_DATE) VALUES $placeholders"
+        stmt = SQLite.Stmt(db, query)
+        params = collect(Iterators.flatten(rows))
+        DBInterface.execute(stmt, params)
 
-    columns = join(schema.names, " | ")
-    _type = join(schema.types, " | ")
-    @info "TABLE TRANSACTIONS_02 IS AVAILABLE:" columns _type
+        columns = join(schema.names, " | ")
+        _type = join(schema.types, " | ")
+        @info "TABLE TRANSACTIONS_02 IS AVAILABLE:" columns _type
+
+    else
+
+        @info "TABLE TRANSACTIONS_02 ALREADY EXISTS"
+
+    end
 
 end
 #=
@@ -94,35 +109,42 @@ end
 =#
 function items_03(db::SQLite.DB)
 
-    schema = Tables.Schema((:DATES, :ITEM), (String, String))
-    SQLite.createtable!(db, "ITEMS_03" , schema, temp = false)
-    
-    rows = [("01-jan-20",
-             "apple"),
-            ("01-jan-20",
-             "apple"),
-            ("01-jan-20",
-             "pear"),
-            ("01-jan-20",
-             "pear"),
-            ("02-jan-20",
-             "pear"),
-            ("02-jan-20",
-             "pear"),
-            ("02-jan-20",
-             "pear"),
-            ("02-jan-20",
-             "orange")]
-    
-    placeholders = join(["(?, ?)" for _ in rows], ", ")
-    query = "INSERT INTO ITEMS_03 (DATES, ITEM) VALUES $placeholders"
-    stmt = SQLite.Stmt(db, query)
-    params = collect(Iterators.flatten(rows))
-    DBInterface.execute(stmt, params)
-    
-    columns = join(schema.names, " | ")
-    _type = join(schema.types, " | ")
-    @info "TABLE ITEMS_03 IS AVAILABLE:" columns _type
+    if !SQLiteArrowKit.is_available(db, "items_03")
+        schema = Tables.Schema((:DATES, :ITEM), (String, String))
+        SQLite.createtable!(db, "ITEMS_03" , schema, temp = false)
+
+        rows = [("01-jan-20",
+                 "apple"),
+                ("01-jan-20",
+                 "apple"),
+                ("01-jan-20",
+                 "pear"),
+                ("01-jan-20",
+                 "pear"),
+                ("02-jan-20",
+                 "pear"),
+                ("02-jan-20",
+                 "pear"),
+                ("02-jan-20",
+                 "pear"),
+                ("02-jan-20",
+                 "orange")]
+
+        placeholders = join(["(?, ?)" for _ in rows], ", ")
+        query = "INSERT INTO ITEMS_03 (DATES, ITEM) VALUES $placeholders"
+        stmt = SQLite.Stmt(db, query)
+        params = collect(Iterators.flatten(rows))
+        DBInterface.execute(stmt, params)
+
+        columns = join(schema.names, " | ")
+        _type = join(schema.types, " | ")
+        @info "TABLE ITEMS_03 IS AVAILABLE:" columns _type
+
+    else
+
+        @info "TABLE ITEMS_03 ALREADY EXISTS"
+
+    end
 
 end
 #=
@@ -130,51 +152,58 @@ end
 =#
 function users_04(db::SQLite.DB)
 
-    schema = Tables.Schema((:ID, :ACTIONS, :ACTION_DATE), (Int32, String, String))
-    SQLite.createtable!(db, "USERS_04" , schema, temp = false)
+    if !SQLiteArrowKit.is_available(db, "users_04")
+        schema = Tables.Schema((:ID, :ACTIONS, :ACTION_DATE), (Int32, String, String))
+        SQLite.createtable!(db, "USERS_04" , schema, temp = false)
 
-    rows = [(1,
-             "Start",
-             "13-feb-20"),
-        
-            (1,
-             "Cancel",
-             "13-feb-20"),
-        
-            (2,
-             "Start",
-             "11-feb-20"),
-        
-            (2,
-             "Publish",
-             "14-feb-20"),
-        
-            (3,
-             "Start",
-             "15-feb-20"),
-        
-            (3,
-             "Cancel",
-             "15-feb-20"),
-        
-            (4,
-             "Start",
-             "18-feb-20"),
-        
-            (1,
-             "Publish",
-             "19-feb-20")]
+        rows = [(1,
+                 "Start",
+                 "13-feb-20"),
 
-    placeholders = join(["(?, ?, ?)" for _ in rows], ", ")
-    query = "INSERT INTO USERS_04 (ID, ACTIONS, ACTION_DATE) VALUES $placeholders"
-    stmt = SQLite.Stmt(db, query)
-    params = collect(Iterators.flatten(rows))
-    DBInterface.execute(stmt, params)
-    
-    columns = join(schema.names, " | ")
-    _type = join(schema.types, " | ")
-    @info "TABLE USERS_04 IS AVAILABLE:" columns _type
-    
+                (1,
+                 "Cancel",
+                 "13-feb-20"),
+
+                (2,
+                 "Start",
+                 "11-feb-20"),
+
+                (2,
+                 "Publish",
+                 "14-feb-20"),
+
+                (3,
+                 "Start",
+                 "15-feb-20"),
+
+                (3,
+                 "Cancel",
+                 "15-feb-20"),
+
+                (4,
+                 "Start",
+                 "18-feb-20"),
+
+                (1,
+                 "Publish",
+                 "19-feb-20")]
+
+        placeholders = join(["(?, ?, ?)" for _ in rows], ", ")
+        query = "INSERT INTO USERS_04 (ID, ACTIONS, ACTION_DATE) VALUES $placeholders"
+        stmt = SQLite.Stmt(db, query)
+        params = collect(Iterators.flatten(rows))
+        DBInterface.execute(stmt, params)
+
+        columns = join(schema.names, " | ")
+        _type = join(schema.types, " | ")
+        @info "TABLE USERS_04 IS AVAILABLE:" columns _type
+
+    else
+
+        @info "TABLE USERS_04 ALREADY EXISTS"
+
+    end
+
 end
 #=
 **********************************************
