3cefb9a4d6de7467cd8b4676fe5890d5a1b1455d
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-25 18:18:31 -0600

    Update query.py

--- 05_super_users/query.py
+++ 05_super_users/query.py
@@ -1,1 +1,106 @@
+import adbc_driver_sqlite.dbapi as dbapi
+import polars as pol
+import pyarrow as pa
+import arrowkit
+import pandas as pd
+import duckdb
 
+def main(table:str):
+
+    conn = dbapi.connect("file:/content/my_SQLite.db?mode=ro")
+    name = table.upper()
+    if (name == "USERS_05"):
+
+        try:
+
+            duck = duckdb.connect(":memory:")
+            arrow_users = arrowkit.get_ArrowTable(conn, table)
+            users = pol.from_arrow(arrow_users).lazy()
+
+            sample = users.head(5)
+            print(":" * 40)
+            print(f'USERS TABLE, USING POLARS LAZYFRAMES -> SAMPLE:\n{sample.collect()}')
+
+            super = (users.select(pol.col("USER_ID"),
+                                  pol.col("TRANSACTION_DATE")
+                           )
+                          .with_columns(pol.col("TRANSACTION_DATE")
+                                           .str
+                                           .strptime(pol.Date,
+                                                     format = "%d-%b-%y"
+                                            )
+                           )
+                          .sort(by = ["USER_ID", "TRANSACTION_DATE"])
+                          .with_columns(pol.col("TRANSACTION_DATE")
+                                           .rank(method = "ordinal")
+                                           .over(partition_by = "USER_ID")
+                                           .alias("TRANSACTION")
+                           )
+                          .filter(pol.col("TRANSACTION") == 2)
+            )
+            users = (users.select(pol.col("USER_ID"))
+                          .unique()
+            )
+            result = (users.join(super,
+                                 on = "USER_ID",
+                                 how = "left"
+                            )
+            )
+            print(f'USERS BECOMING SUPERUSERS, USING POLARS FUNCTIONS:\n{result.collect()}')
+
+            query = """
+                    WITH
+                        DUCK_FORMATTED AS (
+                            SELECT
+                                USER_ID,
+                                DATE(STRPTIME(TRANSACTION_DATE, '%d-%b-%y')) AS TRANSACTION_DATE
+                            FROM
+                                'arrow_users'),
+                        RANKINGS AS (
+                            SELECT
+                                USER_ID,
+                                TRANSACTION_DATE,
+                                ROW_NUMBER() OVER(PARTITION BY
+                                                      USER_ID
+                                                  ORDER BY
+                                                      TRANSACTION_DATE) AS RANKED_DATE
+                            FROM
+                                DUCK_FORMATTED),
+                        USER_ AS (
+                            SELECT DISTINCT
+                                USER_ID
+                            FROM
+                                'arrow_users'),
+                        SUPERUSERS AS (
+                            SELECT
+                                USER_ID,
+                                TRANSACTION_DATE AS DATE_AS_SUPER
+                            FROM
+                                RANKINGS
+                            WHERE
+                                RANKED_DATE = 2)
+            SELECT
+                U.USER_ID,
+                S.DATE_AS_SUPER
+            FROM
+                USER_      U
+                LEFT JOIN
+                    SUPERUSERS S
+                USING (USER_ID)
+            ORDER BY
+                2
+            """
+            duck_result = duck.sql(query).fetch_arrow_table()
+            df = duck_result.to_pandas()
+            print(":" * 40)
+            print(f'USERS BECOMING SUPERUSERS, USING DUCKDB QUERIES:\n{duck_result}')
+            print(f'<*pandas visualization*>\n{df}')
+
+        finally:
+
+            conn.close()
+            duck.close()
+
+    else:
+
+        print(f'TABLE {table} NOT AVAILABLE')
