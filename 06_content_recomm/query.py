67d841b2e6ca0a186deffb96300f1754e252df1c
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-06-26 11:37:59 -0600

    Update query.py

--- 06_content_recomm/query.py
+++ 06_content_recomm/query.py
@@ -1,76 +1,86 @@
-# python -m pip install oracledb numpy pyarrow polars --upgrade
-import numpy as np
-import polars as pl
-import oracledb
-import pyarrow
+import adbc_driver_sqlite.dbapi as dbapi
+import polars as pol
+import pyarrow as pa
+import arrowkit
+import pandas as pd
+import duckdb
 
-try:
-    # You need to write your credentials in the following connection.
-    conn = oracledb.connect(user = "[Username]", password = "[Password]", dsn = "localhost:1521/FREEPDB1")
-    table1 = "SELECT * FROM FRIENDS_P6"
-    table2 = "SELECT * FROM LIKES_P6"
-    odf1 = conn.fetch_df_all(statement = table1, arraysize = 100)
-    odf2 = conn.fetch_df_all(statement = table2, arraysize = 100)
-    pyarrow_table1 = pyarrow.Table.from_arrays(odf1.column_arrays(), names = odf1.column_names())
-    pyarrow_table2 = pyarrow.Table.from_arrays(odf2.column_arrays(), names = odf2.column_names())
-    friends = pl.from_arrow(pyarrow_table1).lazy()
-    likes = pl.from_arrow(pyarrow_table2).lazy()
+def main(table1:str, table2:str):
 
-    '''
-    Alternative 2: Querying directly from this repository 
-    url_1 = "https://raw.githubusercontent.com/Uriel1201/HelloDATA/refs/heads/main/06_content_recomm/data_friends.tsv"
-    url_2 = "https://raw.githubusercontent.com/Uriel1201/HelloDATA/refs/heads/main/06_content_recomm/data_likes.tsv"
-    friends = pl.scan_csv(url_1,
-                          separator = "\t",
-                          has_header = True,
-                          infer_schema_length = 1000,
-                          ignore_errors = False
-                 )
-    likes = pl.scan_csv(url_2,
-                        separator = "\t",
-                        has_header = True,
-                        infer_schema_length = 1000,
-                        ignore_errors = False
-               )
-    '''
-    s_friends = friends.head(5)
-    s_likes = likes.head(5)
-    print(f'\n FRIENDS TABLE (SAMPLE):\n{s_friends.collect()}')
-    print(f'\n LIKES TABLE (SAMPLE):\n{s_likes.collect()}')
+    conn = dbapi.connect("file:/content/my_SQLite.db?mode=ro")
+    name1 = table1.upper()
+    name2 = table2.upper()
+    if (name1 == "FRIENDS_06") and (name2 == "LIKES_06"):
 
-    s_lf = (s_friends.join(s_likes.select(pl.col('USER_ID')
-                                            .alias('FRIEND'),
-                                          pl.col('PAGE_LIKES')
-                                            .alias('RECOMMENDATION?')
-                                   )
-                           ,on = 'FRIEND'
-                           ,how = 'left'
-                      )
-           )
-    print(f'\n IDENTIFYING POTENTIAL RECOMMENDATIONS (SAMPLE):\n{s_lf.collect()}')
-    
-    lf = (friends.join(likes.select(pl.col('USER_ID')
-                                      .alias('FRIEND'),
-                                    pl.col('PAGE_LIKES')
-                             )
-                       ,on = 'FRIEND'
-                       ,how = 'left'
-                  )
-                 .select(pl.col('USER_ID'),
-                         pl.col('PAGE_LIKES')
-                  )
-         )
-    recommendations = (lf.join(likes,
-                               on = ['USER_ID','PAGE_LIKES'],
-                               how = 'anti'
-                          )
-                         .unique()
-                         .select(pl.col('USER_ID'),
-                                 pl.col('PAGE_LIKES')
-                                   .alias('RECOMMENDATION')
-                          )
-                         .sort(by = 'USER_ID')
-                      )
-    print(f'\n RECOMMENDATIONS:\n{recommendations.collect()}')          
-finally:
-    conn.close()
+        try:
+
+            duck = duckdb.connect(":memory:")
+            arrow_friends = arrowkit.get_ArrowTable(conn, table1)
+            arrow_likes = arrowkit.get_ArrowTable(conn, table2)
+
+            friends = pol.from_arrow(arrow_friends).lazy()
+            likes = pol.from_arrow(arrow_likes).lazy()
+
+            sample1 = friends.head(5)
+            sample2 = likes.head(5)
+            print(":" * 40)
+            print(f'FRIENDS TABLE (POLARS) -> SAMPLE:\n{sample1.collect()}')
+            print(f'LIKES TABLE (POLARS) -> SAMPLE:\n{sample2.collect()}')
+
+            result = (friends.join(likes,
+                                   left_on = "FRIEND",
+                                   right_on = "USER_ID",
+                                   coalesce = True
+                              )
+                             .select(pol.col("USER_ID"), 
+                                     pol.col("PAGE_LIKES")
+                                        .alias("RECOMMENDATION")
+                              )
+                             .join(likes, 
+                                   left_on = ["USER_ID", "RECOMMENDATION"], 
+                                   right_on = ["USER_ID", "PAGE_LIKES"], 
+                                   coalesce = True, 
+                                   how = "anti"
+                              )
+                             .unique()
+            )
+            print(":" * 40)
+            print(f'RETURNING RECOMMENDATIONS FOR EACH USER (POLARS):\n{result.collect()}')
+
+            query = """
+                    WITH
+                        RECOMMENDATIONS AS (
+                            SELECT
+                                F.USER_ID,
+                                L.PAGE_LIKES AS RECOMMENDATION
+                            FROM
+                                'arrow_friends' F
+                                INNER JOIN 'arrow_likes' L
+                                    ON F.FRIEND = L.USER_ID)
+            SELECT DISTINCT
+                R.USER_ID,
+                R.RECOMMENDATION
+            FROM
+                RECOMMENDATIONS R
+                ANTI JOIN 'arrow_likes' L
+                    ON
+                        R.USER_ID = L.USER_ID
+                    AND
+                        R.RECOMMENDATION = L.PAGE_LIKES
+            ORDER BY
+                1, 2
+            """
+            duck_result = duck.sql(query).fetch_arrow_table()
+            df = duck_result.to_pandas()
+            print(":" * 40)
+            print(f'RETURNING RECOMMENDATIONS FOR EACH USER (DUCKDB QUERIES):\n{duck_result}')
+            print(f'<*pandas visualization*>\n{df}')
+
+        finally:
+
+            conn.close()
+            duck.close()
+
+    else:
+
+        print(f'TABLE {table1} OR TABLE {table2} NOT AVAILABLE')
