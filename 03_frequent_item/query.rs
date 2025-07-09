2a9266717cb75c582ce715bf730915fb6f102e36
Dante1201 <128760419+Uriel1201@users.noreply.github.com>
2025-07-09 15:34:36 -0600

    Update query.rs

--- 03_frequent_item/query.rs
+++ 03_frequent_item/query.rs
@@ -1,33 +1,53 @@
-use rusqlite::{Connection, Result, params}; // 0.35.0
+use rusqlite::{Connection, Result as RusqliteResult, params}; // 0.35.0
 use chrono::NaiveDate; // 0.4.41
 use thiserror::Error;
+use std::collections::HashMap;
 
 #[derive(Error, Debug)]
 pub enum AppError {
-    #[error("Error de base de datos: {0}")]
+    #[error("Error in Database: {0}")]
     Database(#[from] rusqlite::Error),
-    #[error("Error de fecha: {0}")]
+    #[error("Date Error: {0}")]
     DateParse(#[from] chrono::ParseError),
 }
 
 #[derive(Debug)]
-struct Items {
+struct Item {
     item: String, 
-    dates: Option<NaiveDate>,
+    date: String,
+}
+#[derive(Debug)]
+struct ItemF {
+    item: String, 
+    date: Option<NaiveDate>,
+}
+#[derive(Debug)]
+struct Count {
+    item: String, 
+    count: usize,
 }
 
-impl Items {
-    fn new(item: &str, date_str: &str) -> Result<Self, chrono::ParseError> {
-        Ok(Self {
+impl Item {
+    fn new(item: &str, date_str: &str) -> Self {
+        Self {
             item: item.to_string(),
-            dates: Some(NaiveDate::parse_from_str(date_str, "%d-%b-%y")?),
-        })
+            date: date_str.to_string(),
+        }
     }
 }
 
-macro_rules! items {
-    ($item:expr, $dates:expr) => {
-        Items::new($item, $dates)?
+impl Count {
+    fn new(item: String,  count: usize) -> Self {
+        Self {
+            item,
+            count,
+        }
+    }
+}
+
+macro_rules! item {
+    ($item:expr, $date:expr) => {
+        Item::new($item, $date)
     };
 }
 
@@ -37,95 +57,101 @@ fn main() -> Result<(), AppError> {
     
     conn.execute(
     "CREATE TABLE ITEMS(
-         DATES DATE,
+         DATE VARCHAR(9),
          ITEM VARCHAR(9)
      )", (),
     )?;
     
     let items = vec![
-    items!("apple", "01-jan-20"),
-    items!("apple", "01-jan-20"),
-    items!("pear", "01-jan-20"),
-    items!("pear", "01-jan-20"),
-    items!("pear", "02-jan-20"),
-    items!("pear", "02-jan-20"),
-    items!("pear", "02-jan-20"),
-    items!("orange", "02-jan-20"),
+    item!("apple", "01-jan-20"),
+    item!("apple", "01-jan-20"),
+    item!("pear", "01-jan-20"),
+    item!("pear", "01-jan-20"),
+    item!("pear", "02-jan-20"),
+    item!("pear", "02-jan-20"),
+    item!("pear", "02-jan-20"),
+    item!("orange", "02-jan-20"),
+    item!("Dewey", "fuaaaa!!!"),
+    item!("Thanos", "67-jan-93"),
     ];
     
     let tx = conn.transaction()?;
     {
-    let mut stmt = tx.prepare("INSERT INTO ITEMS(DATES, ITEM) 
+    let mut stmt = tx.prepare("INSERT INTO ITEMS(DATE, ITEM) 
                                values (?1, ?2)")?;
     for i in items {
-        stmt.execute(params![i.dates, i.item])?;
+        stmt.execute(params![i.date, i.item])?;
     }
     }
     tx.commit()?;
     
-    {
-    println!("ITEMS SAMPLE(5):");
-    let mut stmt = conn.prepare("SELECT * FROM ITEMS LIMIT 5")?;
-    let items_iter = stmt.query_map([], |row| {Ok(Items {
+    
+    println!("--- ITEM'S DATA ---");
+    let mut stmt = conn.prepare("SELECT
+                                     DATE, 
+                                     ITEM
+                                 FROM
+                                     ITEMS"
+                        )?;
+                        
+    let items_iter = stmt.query_map([], |row| {Ok(Item {
                                                       item: row.get(1)?,
-                                                      dates: row.get(0)?,
+                                                      date: row.get(0)?,
                                                   }
                                                )
                                               })?;
     for i in items_iter {
         println!("Found: {:?}", i.unwrap());
     }
+    
+    let items_iter = stmt.query_map([], 
+                                    |row| {
+                                           let date_str: String = row.get("DATE")?;
+                                           let parsed_date = NaiveDate::parse_from_str(&date_str, 
+                                                                                       "%d-%b-%y"
+                                                                        ).map_err(|e| rusqlite::Error::FromSqlConversionFailure(0, 
+                                                                                                                                rusqlite::types::Type::Text, 
+                                                                                                                                Box::new(e)
+                                                                                                       )
+                                                                          )?;
+    
+                                               RusqliteResult::Ok(ItemF {
+                                                                         item: row.get("ITEM")?,
+                                                                         date: Some(parsed_date),
+                                                                        }
+                                                                )
+                                          }
+                            )?; 
+                 
+    let mut all_items_v:Vec<ItemF> = Vec::new();
+    for i in items_iter {
+        match i {
+            Ok(item_) => all_items_v.push(item_),
+            Err(e) => eprintln!("Hi my friend!, there's a problem: {:?}",
+                                AppError::Database(e)
+                      ),
+        }
     }
     
-    {
-    let mut stmt = conn.prepare(
-    "WITH FREQUENCIES AS (
-        SELECT
-            DATES,
-            ITEM,
-            COUNT(*) AS FREQUENCY 
-        FROM
-            ITEMS
-        GROUP BY
-            DATES,
-            ITEM
-    ), ITEMS_RANKING AS (
-        SELECT
-            DATES,
-            ITEM,
-            RANK()
-            OVER(PARTITION BY DATES
-                 ORDER BY
-                     FREQUENCY DESC
-            ) AS RANK
-        FROM
-            FREQUENCIES
-    )
-    SELECT
-        DATES,
-        ITEM
-    FROM
-        ITEMS_RANKING
-    WHERE
-        RANK = 1"
-    )?;
-    println!("\nMOST FREQUENTED ITEMS:");
-    
-    let max_iter = stmt.query_map([], |row| {Ok(Items {
-                                                    item: row.get("ITEM")?,
-                                                    dates: row.get("DATES")?,
-                                                }
-                                               )}
-                                 )?;
-                                     
-    for ma in max_iter {
-        let ma = ma?;
-        println!("Item: {:?} -> in date: {:?}",
-                 ma.item,
-                 ma.dates
-        );
-    }
+    let mut items_grouped: HashMap<(String, Option<NaiveDate>), Vec<ItemF>> = HashMap::new();
+    for i in all_items_v {
+        items_grouped.entry((i.item.clone(), i.date)).or_insert_with(Vec::new).push(i);
     }
     
+    let mut item_count: HashMap<Option<NaiveDate>, Vec<Count>> = HashMap::new();
+    for (i,list) in items_grouped {
+        item_count.entry(i.1).or_insert_with(Vec::new).push(Count::new(i.0, list.len()));
+    }
+    
+    println!("\n-- MOST FREQUENTED ITEM BY EACH DATE --");
+    for (i, list) in &item_count {
+        let max_count = list.iter().map(|item| item.count).max();
+        let max_items: Vec<&Count> = list.iter().filter(|item| {item.count == max_count.unwrap_or(0)}).collect();
+        println!("date: {:?}", i);
+        for item in max_items {
+            println!("    item: {}; count: {}", item.item, item.count);
+        }
+    }
+
     Ok(())
 }
